5c3d669eec8761d65a7a9fce0c2708fea2c55c6e
zkfy <lib4net@163.com>
2018-05-14 15:37:23 +0800

    修改request

--- examples/rpcserver/services/user/login.go
+++ examples/rpcserver/services/user/login.go
@@ -15,7 +15,7 @@ func NewLoginHandler(container component.IContainer) (u *LoginHandler) {
 }
 func (u *LoginHandler) Handle(ctx *context.Context) (r interface{}) {
 	//检查用户名密码是否正确
-	ctx.Response.SetJWTBody(map[string]interface{}{
+	ctx.Response.SetJWT(map[string]interface{}{
 		"id": 11000,
 	})
 	return "ok"
