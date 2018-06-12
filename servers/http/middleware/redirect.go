6f0918cd941f52f2c8e66f1032e8fdc7d9947c03
zkfy <lib4net@163.com>
2018-06-12 18:14:17 +0800

    .

---
+++ servers/http/middleware/redirect.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/micro-plat/hydra/conf"
+)
+
+func Redirect(cnf *conf.MetadataConf) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Next()
+		context := getCTX(ctx)
+
+		if context == nil {
+			return
+		}
+		defer context.Close()
+		//处理跳转3xx
+		if url, ok := context.Response.IsRedirect(); ok {
+			ctx.Redirect(context.Response.GetStatus(), url)
+			return
+		}
+	}
+}
