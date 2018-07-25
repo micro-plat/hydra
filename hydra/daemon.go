090738bbbce616926712550d152443cabfcf2928
zkfy <lib4net@163.com>
2018-07-25 10:10:59 +0800

    优化服务安装程序

--- hydra/daemon.go
+++ hydra/daemon.go
@@ -2,31 +2,34 @@
 package hydra
 
 import (
-	"fmt"
+	"bytes"
 	"os"
 
 	"github.com/micro-plat/hydra/conf/creator"
 	"github.com/micro-plat/hydra/registry"
 	"github.com/micro-plat/lib4go/logger"
 	"github.com/urfave/cli"
+	"github.com/zkfy/log"
 )
+
+var xlogger = log.New(bytes.NewBufferString(""), "", log.Llongcolor)
 
 func (m *MicroApp) startAction(c *cli.Context) (err error) {
 	msg, err := m.service.Start()
 	if err != nil {
-		fmt.Println(err)
+		xlogger.Error(err)
 		return err
 	}
-	fmt.Println(msg)
+	xlogger.Info(msg)
 	return nil
 }
 func (m *MicroApp) stopAction(c *cli.Context) (err error) {
 	msg, err := m.service.Stop()
 	if err != nil {
-		fmt.Println(err)
+		xlogger.Error(err)
 		return err
 	}
-	fmt.Println(msg)
+	xlogger.Info(msg)
 	return nil
 }
 func (m *MicroApp) installAction(c *cli.Context) (err error) {
@@ -38,39 +41,38 @@ func (m *MicroApp) installAction(c *cli.Context) (err error) {
 	}
 	//安装配置服务
 	p := func(v ...interface{}) {
-		fmt.Println(v...)
+		xlogger.Info(v)
 	}
 	if err = m.install(p); err != nil {
-		fmt.Println(err)
+		xlogger.Error(err)
 		return err
 	}
 
 	//安装配置文件
 	msg, err := m.service.Install(os.Args[2:]...)
 	if err != nil {
-		fmt.Println(err)
+		xlogger.Error(err)
 		return err
 	}
-
-	fmt.Println(msg)
+	xlogger.Info(msg)
 	return nil
 }
 func (m *MicroApp) removeAction(c *cli.Context) (err error) {
 	msg, err := m.service.Remove()
 	if err != nil {
-		fmt.Println(err)
+		xlogger.Error(err)
 		return err
 	}
-	fmt.Println(msg)
+	xlogger.Info(msg)
 	return nil
 }
 func (m *MicroApp) statusAction(c *cli.Context) (err error) {
 	msg, err := m.service.Status()
 	if err != nil {
-		fmt.Println(err)
+		xlogger.Error(err)
 		return err
 	}
-	fmt.Println(msg)
+	xlogger.Info(msg)
 	return nil
 }
 
@@ -80,7 +82,7 @@ func (m *MicroApp) install(p func(v ...interface{})) (err error) {
 	//创建注册中心
 	rgst, err := registry.NewRegistryWithAddress(m.RegistryAddr, m.logger)
 	if err != nil {
-		m.logger.Error(err)
+		xlogger.Error(err)
 		return err
 	}
 
@@ -88,6 +90,11 @@ func (m *MicroApp) install(p func(v ...interface{})) (err error) {
 	vlogger := logger.New("creator")
 	vlogger.DoPrint = p
 	creator := creator.NewCreator(m.PlatName, m.SystemName, m.ServerTypes, m.ClusterName, m.Conf, m.RegistryAddr, rgst, vlogger)
-	return creator.Start()
+	err = creator.Start()
+	if err != nil {
+		xlogger.Error(err)
+		return err
+	}
+	return nil
 
 }
