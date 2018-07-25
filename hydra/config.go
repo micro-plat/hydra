29f2a87bddd03df7afcc2644e36a24fd018546f3
zkfy <lib4net@163.com>
2018-07-25 15:07:39 +0800

    优化服务安装程序

--- hydra/config.go
+++ hydra/config.go
@@ -32,7 +32,7 @@ func (m *MicroApp) queryConfigAction(c *cli.Context) (err error) {
 		if err != nil {
 			return err
 		}
-		_, err := conf.NewServerConf(mainPath, buffer, version, rgst)
+		_, err = conf.NewServerConf(mainPath, buffer, version, rgst)
 		if err != nil {
 			return err
 		}
