f5404320531b904a41712750a5491193cb4de442
zkfy <lib4net@163.com>
2018-08-22 16:38:14 +0800

    修改rpc日志编码格式

---
+++ examples/rlogserver/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/micro-plat/hydra/context"
+
+	"github.com/micro-plat/hydra/hydra"
+)
+
+func main() {
+	app := hydra.NewApp(
+		hydra.WithPlatName("myplat_test_rlogger"),
+		hydra.WithSystemName("demo"),
+		hydra.WithServerTypes("api"),
+		hydra.WithRemoteLogger(),
+		hydra.WithDebug())
+
+	app.Conf.API.SetMainConf(`{"address":":7890"}`)
+	app.Conf.Plat.SetVarConf("global", "logger", `{
+    "level":"All",
+		"interval":"1s",
+		"layout":{"name":"%name","time":"%datetime","content":"%content","level":"%l","session":"%session"},
+    "service":"/hydra/log/save@logging.logging_debug"
+}
+	`)
+
+	app.Micro("/hello", helloWorld)
+	app.Start()
+}
+
+func helloWorld(ctx *context.Context) (r interface{}) {
+	return "hello world"
+}
