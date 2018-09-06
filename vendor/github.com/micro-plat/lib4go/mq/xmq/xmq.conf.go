3af48fcfe2dfa076070d26707adada4197f64675
zkfy <lib4net@163.com>
2018-09-06 15:05:51 +0800

    .

---
+++ vendor/github.com/micro-plat/lib4go/mq/xmq/xmq.conf.go
@@ -0,0 +1,25 @@
+package xmq
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/asaskevich/govalidator"
+)
+
+type Conf struct {
+	Address string `json:"address" valid:"dialstring,required"`
+}
+
+func NewConf(j string) (*Conf, error) {
+	conf := Conf{}
+	err := json.Unmarshal([]byte(j), &conf)
+	if err != nil {
+		return nil, err
+	}
+	if b, err := govalidator.ValidateStruct(&conf); !b {
+		err = fmt.Errorf("xmq 配置文件有误:%v", err)
+		return nil, err
+	}
+	return &conf, nil
+}
