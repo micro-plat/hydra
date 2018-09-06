268975e18caf2d36c7c121f706a2ce30f5cecbab
zkfy <lib4net@163.com>
2018-09-06 17:04:36 +0800

    .

--- vendor/github.com/micro-plat/lib4go/queue/xmq/xmq.producer.go
+++ vendor/github.com/micro-plat/lib4go/queue/xmq/xmq.producer.go
@@ -99,14 +99,13 @@ func (producer *XMQProducer) writeMessage(msg string) error {
 	producer.lastWrite = time.Now()
 	result, err := encoding.ConvertBytes([]byte(msg), "gbk")
 	if err != nil {
-		//producer.disconnect()
 		return err
 	}
 	_, err = producer.conn.Write(result)
 	producer.lastWrite = time.Now()
-	if err != nil {
-		producer.disconnect()
-	}
+	// if err != nil {
+	// 	producer.disconnect()
+	// }
 	return err
 }
 
