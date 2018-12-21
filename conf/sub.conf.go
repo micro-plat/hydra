e9c3e6d81580ab332af2ddbaeb5d01ebb8fbc1ee
liujinyin <liujy@100bm.cn>
2018-12-21 16:58:46 +0800

    auth.exclude 配置，增加模糊匹配模式       1. /a/b/ *       2. /a/ **       3. /a/ * /d

--- conf/sub.conf.go
+++ conf/sub.conf.go
@@ -1,5 +1,9 @@
 package conf
 
+import (
+	"strings"
+)
+
 type HttpServerConf struct {
 	Address           string `json:"address" valid:"dialstring"`
 	Status            string `json:"status" valid:"in(start|stop)"`
@@ -40,6 +44,70 @@ type Auth struct {
 	Domain     string   `json:"domain" valid:"ascii"`
 	Disable    bool     `json:"disable"`
 }
+
+var cacheExcludeSvcs = map[string]bool{}
+
+//IsExcluded 是否是排除验证的服务
+func (a *Auth) IsExcluded(service string) bool {
+
+	service = strings.ToLower(service)
+	if v, e := cacheExcludeSvcs[service]; e && v {
+		return true
+	}
+
+	sparties := strings.Split(service, "/")
+	//排除指定请求
+	for _, u := range a.Exclude {
+		//完全匹配
+		if strings.EqualFold(u, service) {
+			cacheExcludeSvcs[service] = true
+			return true
+		}
+		//分段模糊
+		uparties := strings.Split(u, "/")
+		//取较少的数组长度
+		uc := len(uparties)
+		sc := len(sparties)
+		/*
+			处理模式：
+			1. /a/b/ *
+			2. /a/ **
+			3. /a/ * /d
+		**/
+		if uc != sc && !strings.HasSuffix(u, "**") {
+			continue
+		}
+		if uc > sc {
+			continue
+		}
+		isMatch := true
+		for i := 0; i < uc; i++ {
+			if uparties[i] == "**" {
+				cacheExcludeSvcs[service] = true
+				return true
+			}
+			if uparties[i] == "*" {
+				for j := i + 1; j < uc; j++ {
+					if uparties[j] != sparties[j] {
+						isMatch = false
+						break
+					}
+				}
+				if !isMatch {
+					break
+				}
+				cacheExcludeSvcs[service] = true
+				return true
+			}
+			if uparties[i] != sparties[i] {
+				break
+			}
+		}
+
+	}
+	return false
+}
+
 type Routers struct {
 	Setting map[string]string `json:"args"`
 	Routers []*Router         `json:"routers"`
