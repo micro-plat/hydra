b38330d2e5c063d6d502d84484ff501fec759f47
yanglei <yanglei@100bm.cn>
2019-01-09 16:05:00 +0800

    修改metrics上报时的服务器字段名称

---
+++ examples/metricserver/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/micro-plat/hydra/context"
+
+	"github.com/micro-plat/hydra/hydra"
+)
+
+func main() {
+	app := hydra.NewApp(
+		hydra.WithPlatName("myplat_test_9_2"),
+		hydra.WithSystemName("demo"),
+		hydra.WithServerTypes("api-cron-rpc-web-ws"),
+	)
+
+	app.Conf.API.SetMainConf(`{"address":":9067"}`)
+	app.Conf.API.SetSubConf("metric", `{
+		"host":"http://192.168.106.219:8086",
+	"dataBase":"hydra_metrics",
+	"cron":"@every 10s",
+	"userName":"",
+	"password":""
+	}	`)
+
+	app.Micro("/hello", helloWorld)
+	app.Start()
+}
+
+func helloWorld(ctx *context.Context) (r interface{}) {
+	return "hello"
+}
