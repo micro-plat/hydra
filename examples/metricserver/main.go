fe688209ff4469ce7d856fbf42613cf3769e8b77
yanglei <yanglei@100bm.cn>
2019-01-09 17:31:15 +0800

    修改BUG:请求异常消息未记录到日志文件中

--- examples/metricserver/main.go
+++ examples/metricserver/main.go
@@ -10,26 +10,23 @@ func main() {
 	app := hydra.NewApp(
 		hydra.WithPlatName("myplat_test_9_2"),
 		hydra.WithSystemName("demo"),
-		hydra.WithServerTypes("cron"),
+		hydra.WithServerTypes("api"),
 	)
 
-	app.Conf.CRON.SetMainConf(`{"address":":9067"}`)
-	app.Conf.CRON.SetSubConf("metric", `{
+	app.Conf.API.SetMainConf(`{"address":":9067"}`)
+	app.Conf.API.SetSubConf("metric", `{
 		"host":"http://192.168.106.219:8086",
 	"dataBase":"hydra_metrics",
 	"cron":"@every 10s",
 	"userName":"",
 	"password":""
 	}	`)
-	app.Conf.CRON.SetSubConf("task", `{"tasks":[
-			{"cron":"@every 10s","service":"/hello"}			
-	]}`)
 
-	app.CRON("/hello", helloWorld)
+	app.API("/hello", helloWorld)
 	app.Start()
 }
 
 func helloWorld(ctx *context.Context) (r interface{}) {
-	panic("hello.err")
-	//	return "hello"
+	//panic("hello.err")
+	return "hello"
 }
