b7a016f0f00d0e4bd3cdb6c474d462df2d4c0063
yanglei <yanglei@100bm.cn>
2019-01-09 19:13:53 +0800

    添加启动模式枚举

--- hydra/args.go
+++ hydra/args.go
@@ -8,6 +8,7 @@ type ArgContext struct {
 	*cli.Context
 	cmds     []cli.Flag
 	Validate func() error
+	RunMode  int
 }
 
 func newArgsContext() *ArgContext {
@@ -20,6 +21,23 @@ func newArgsContext() *ArgContext {
 }
 func (a *ArgContext) setCtx(c *cli.Context) {
 	a.Context = c
+	switch c.Command.Name {
+	case "install":
+		a.RunMode = ModeInstall
+	case "run":
+		a.RunMode = ModeRun
+	case "start":
+		a.RunMode = ModeStart
+	case "stop":
+		a.RunMode = ModeStop
+	case "status":
+		a.RunMode = ModeStatus
+	case "conf":
+		a.RunMode = ModeConf
+	case "remove":
+		a.RunMode = ModeRemove
+
+	}
 }
 func (a *ArgContext) Append(c cli.Flag) {
 	a.cmds = append(a.cmds, c)
