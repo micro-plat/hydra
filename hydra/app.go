5f9eb8f41edbbc04a74944a892e4f54eca6c28ad
zkfy <lib4net@163.com>
2019-03-13 15:21:30 +0800

    修改权限检查

--- hydra/app.go
+++ hydra/app.go
@@ -97,7 +97,6 @@ func (m *MicroApp) action(c *cli.Context) (err error) {
 		//创建注册中心
 		rgst, err := registry.NewRegistryWithAddress(m.RegistryAddr, m.logger)
 		if err != nil {
-			m.logger.Error("xfes:")
 			m.logger.Error(err)
 			return err
 		}
