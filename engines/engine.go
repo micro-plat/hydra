12a7301f5bc4918cb8c93de0338a563da890249c
zkfy <lib4net@163.com>
2019-04-19 16:43:10 +0800

    icontainer增加日志对象获取方法

--- engines/engine.go
+++ engines/engine.go
@@ -208,7 +208,7 @@ func appendEngines(engines []string, ext ...string) []string {
 func goid() int {
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println("panic recover:panic info:%v", err)
+			fmt.Println("panic recover:panic info:", err)
 		}
 	}()
 
@@ -217,7 +217,7 @@ func goid() int {
 	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
 	id, err := strconv.Atoi(idField)
 	if err != nil {
-		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
+		return 0
 	}
 	return id
 }
--- examples/apiserver/main.go
+++ examples/apiserver/main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
-	"strconv"
-	"strings"
-
 	"github.com/micro-plat/hydra/context"
 
 	"github.com/micro-plat/hydra/examples/apiserver/services/order"
@@ -25,20 +20,4 @@ func main() {
 	app.Micro("/order/query", order.NewQueryHandler)
 	app.Micro("/order/bind", order.NewBindHandler)
 	app.Start()
-}
-func Goid() int {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println("panic recover:panic info:%v", err)
-		}
-	}()
-
-	var buf [64]byte
-	n := runtime.Stack(buf[:], false)
-	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
-	id, err := strconv.Atoi(idField)
-	if err != nil {
-		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
-	}
-	return id
 }
