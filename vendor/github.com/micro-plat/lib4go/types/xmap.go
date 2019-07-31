d4d280ce4f40b8287c635ffd9cf3012457696e9f
zkfy <lib4net@163.com>
2019-07-31 11:38:49 +0800

    更新lib4go

--- vendor/github.com/micro-plat/lib4go/types/xmap.go
+++ vendor/github.com/micro-plat/lib4go/types/xmap.go
@@ -139,11 +139,7 @@ func (q XMap) GetMustFloat64(name string) (float64, bool) {
 
 //ToStruct 将当前对象转换为指定的struct
 func (q XMap) ToStruct(o interface{}) error {
-	input := make(map[string]interface{})
-	for k, v := range q {
-		input[k] = fmt.Sprint(v)
-	}
-	return Map2Struct(&input, &o)
+	return Map2Struct(q, o)
 }
 
 func (m *XMap) Megre(anr XMap) {
