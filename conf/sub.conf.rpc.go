f0d5dc6dec6136569bf9b5a505076ff4ef99ce93
zkfy <lib4net@163.com>
2019-08-08 14:52:00 +0800

    添加conf配置方法

---
+++ conf/sub.conf.rpc.go
@@ -0,0 +1,36 @@
+package conf
+
+//RPCServerConf api server配置信息
+type RPCServerConf struct {
+	Address   string `json:"address,omitempty" valid:"dialstring"`
+	Status    string `json:"status,omitempty" valid:"in(start|stop)"`
+	RTimeout  int    `json:"readTimeout,omitempty"`
+	WTimeout  int    `json:"writeTimeout,omitempty"`
+	RHTimeout int    `json:"readHeaderTimeout,omitempty"`
+	Trace     bool   `json:"trace,omitempty"`
+}
+
+//NewRPCServerConf 构建api server配置信息
+func NewRPCServerConf(address string) *RPCServerConf {
+	return &RPCServerConf{
+		Address: address,
+	}
+}
+
+//WithTrace 构建api server配置信息
+func (a *RPCServerConf) WithTrace() *RPCServerConf {
+	a.Trace = true
+	return a
+}
+
+//WithDisable 禁用任务
+func (a *RPCServerConf) WithDisable() *RPCServerConf {
+	a.Status = "stop"
+	return a
+}
+
+//WithEnable 启用任务
+func (a *RPCServerConf) WithEnable() *RPCServerConf {
+	a.Status = "start"
+	return a
+}
