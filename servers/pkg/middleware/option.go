c8e817a58f22d13f88079532513a56b882b74d42
zkfy <lib4net@163.com>
2020-04-27 17:49:09 +0800

    修改中间件

---
+++ servers/pkg/middleware/option.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/micro-plat/hydra/servers/pkg/swap"
+)
+
+//Options 请求处理
+func Options() swap.Handler {
+	return func(r swap.IRequest) {
+
+		//options请求则自动不再进行后续处理
+		if strings.ToUpper(r.GetMethod()) == "OPTIONS" {
+			return
+		}
+		r.Next()
+
+	}
+}
