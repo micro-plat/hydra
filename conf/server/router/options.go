cb9e1e3585074ca2794b37c77b08c04689604529
zkfy <lib4net@163.com>
2020-05-29 12:11:17 +0800

    增加非utf-8编码解码

---
+++ conf/server/router/options.go
@@ -0,0 +1,18 @@
+package router
+
+//Option 路由配置选项
+type Option func(*Router)
+
+//WithPages 设置当前服务对应的页面信息
+func WithPages(p ...string) Option {
+	return func(a *Router) {
+		a.Pages = p
+	}
+}
+
+//WithEncoding 设置当前服务对应的页面信息
+func WithEncoding(encoding string) Option {
+	return func(a *Router) {
+		a.Encoding = encoding
+	}
+}
