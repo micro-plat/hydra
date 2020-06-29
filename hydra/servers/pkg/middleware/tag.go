29a54deacd50e8c27a70333458bc50d7027cac0b
zkfy <lib4net@163.com>
2020-06-29 14:05:03 +0800

    增加websocket服务

---
+++ hydra/servers/pkg/middleware/tag.go
@@ -0,0 +1,9 @@
+package middleware
+
+//Tag 添加服务标签
+func Tag() Handler {
+	return func(ctx IMiddleContext) {
+		ctx.Next()
+		ctx.Response().AddSpecial(ctx.ServerConf().GetMainConf().GetServerType())
+	}
+}
