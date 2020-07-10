62468aa7ad4e959a41709a9b23247bd872c81143
zkfy <lib4net@163.com>
2020-07-10 18:05:04 +0800

    添加服务器限流

--- hydra/servers/pkg/middleware/acl.limit.go
+++ hydra/servers/pkg/middleware/acl.limit.go
@@ -23,15 +23,21 @@ func Limit() Handler {
 		}
 
 		//获取执行令牌
-		ctx.Response().AddSpecial("limit")
 		res := rule.GetLimiter().Reserve()
 
 		//判断请求是否需要进行延迟处理
 		delay := res.Delay()
+		if delay <= 0 {
+			ctx.Next()
+			return
+		}
+
+		//当前请求被限流
+		ctx.Response().AddSpecial("limit")
 		wait := rule.GetDelay()
 		if delay > wait { //当前请求将被限流，根据配置进行降级或结果输出处理
 			res.Cancel()
-			ctx.Request().Path().Limit(true, rule.Fallback) //已被限流
+			ctx.Request().Path().Limit(true, rule.Fallback)
 			s, c := rule.GetResponse()
 			ctx.Response().Write(s, c)
 			ctx.Next()
