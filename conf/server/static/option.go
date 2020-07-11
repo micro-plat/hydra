fa25ead6a206d452c3069415967b604d81eaf877
zkfy <lib4net@163.com>
2020-07-11 15:43:03 +0800

    添加docs server

--- conf/server/static/option.go
+++ conf/server/static/option.go
@@ -12,7 +12,7 @@ func newStatic() *Static {
 	a.FirstPage = "index.html"
 	a.Rewriters = []string{"/", "index.htm", "default.html"}
 	a.Exclude = []string{"/views/", ".exe", ".so"}
-	a.Exts = []string{".txt", ".html", ".htm", ".js", ".css", ".map", ".ttf", ".woff", ".woff2", ".woff2", ".jpg", ".jpeg", ".png", ".gif", ".ico", ".tif", ".pcx", ".tga", ".exif", ".fpx", ".svg", ".psd", ".cdr", ".pcd", ".dxf", ".ufo", ".eps", ".ai", ".raw", ".WMF", ".webp"}
+	a.Exts = []string{".json", ".md", ".txt", ".html", ".htm", ".js", ".css", ".map", ".ttf", ".woff", ".woff2", ".woff2", ".jpg", ".jpeg", ".png", ".gif", ".ico", ".tif", ".pcx", ".tga", ".exif", ".fpx", ".svg", ".psd", ".cdr", ".pcd", ".dxf", ".ufo", ".eps", ".ai", ".raw", ".WMF", ".webp"}
 	return a
 }
 
