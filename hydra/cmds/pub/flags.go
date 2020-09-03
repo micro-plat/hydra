c75dbb1426a2a2ce47c840cc5d3202002445de65
zkfy <lib4net@163.com>
2020-09-03 14:39:33 +0800

    修改文件发布逻辑

--- hydra/cmds/pub/flags.go
+++ hydra/cmds/pub/flags.go
@@ -4,28 +4,16 @@ import (
 	"github.com/urfave/cli"
 )
 
-var runRun = false
-var runStart = false
 var runInstall = ""
 var pwd string
 
 //getFlags 获取运行时的参数
 func getFlags() []cli.Flag {
 	flags := make([]cli.Flag, 0, 1)
-	flags = append(flags, cli.BoolFlag{
-		Name:        "start,s",
-		Destination: &runStart,
-		Usage:       `-按需执行start命令`,
-	})
 	flags = append(flags, cli.StringFlag{
 		Name:        "install,i",
 		Destination: &runInstall,
 		Usage:       `-按需执行install命令`,
-	})
-	flags = append(flags, cli.BoolFlag{
-		Name:        "run",
-		Destination: &runRun,
-		Usage:       `-按需执行run命令`,
 	})
 	flags = append(flags, cli.StringFlag{
 		Name:        "pwd",
