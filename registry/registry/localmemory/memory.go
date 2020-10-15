5ca328643b7cadaff35a6a248bab27d3686960d6
zkfy <lib4net@163.com>
2020-10-15 16:16:50 +0800

    添加localmemory单元测试

--- registry/registry/localmemory/memory.go
+++ registry/registry/localmemory/memory.go
@@ -1,29 +1,33 @@
 package localmemory
 
 import (
-	"fmt"
-	"strings"
 	"sync"
-	"sync/atomic"
 
 	r "github.com/micro-plat/hydra/registry"
 	"github.com/micro-plat/lib4go/registry"
 )
 
 //Local 本地内存作为注册中心
-var Local r.IRegistry = newLocalMemory()
+var Local r.IRegistry = NewLocalMemory()
 
 type localMemory struct {
-	closeCh  chan struct{}
-	nodes    map[string]string
-	seqValue int32
-	lock     sync.RWMutex
+	closeCh        chan struct{}
+	nodes          map[string]*value
+	seqValue       int32
+	lock           sync.RWMutex
+	vlock          sync.RWMutex
+	clock          sync.RWMutex
+	valueWatchs    map[string]chan registry.ValueWatcher
+	childrenWatchs map[string]chan registry.ChildrenWatcher
 }
 
-func newLocalMemory() *localMemory {
+func NewLocalMemory() *localMemory {
 	return &localMemory{
-		closeCh: make(chan struct{}),
-		nodes:   make(map[string]string),
+		seqValue:       10000,
+		closeCh:        make(chan struct{}),
+		nodes:          make(map[string]*value),
+		valueWatchs:    make(map[string]chan registry.ValueWatcher),
+		childrenWatchs: make(map[string]chan registry.ChildrenWatcher),
 	}
 }
 
@@ -37,141 +41,8 @@ func (l *localMemory) Exists(path string) (bool, error) {
 	return false, nil
 }
 
-var once sync.Once
-
-func (l *localMemory) print() {
-	once.Do(func() {
-		for k, v := range l.nodes {
-			fmt.Printf("%-48s %-32s\n", k, v)
-		}
-	})
-}
-
-func (l *localMemory) GetValue(path string) (data []byte, version int32, err error) {
-	l.lock.RLock()
-	defer l.lock.RUnlock()
-	vpath := r.Join(path)
-	if v, ok := l.nodes[vpath]; ok {
-		return []byte(v), 0, nil
-	}
-
-	return nil, 0, fmt.Errorf("节点[%s]不存在", vpath)
-
-}
-func (l *localMemory) Update(path string, data string) (err error) {
-	l.lock.RLock()
-	defer l.lock.RUnlock()
-	if _, ok := l.nodes[r.Join(path)]; ok {
-		l.nodes[r.Join(path)] = data
-		return nil
-	}
-	return fmt.Errorf("节点[%s]不存在", path)
-}
-func (l *localMemory) GetChildren(path string) (paths []string, version int32, err error) {
-	l.lock.RLock()
-	defer l.lock.RUnlock()
-	paths = make([]string, 0, 1)
-	npath := r.Join(path)
-	exists := make(map[string]string)
-	for k := range l.nodes {
-		if strings.HasPrefix(k, npath) && len(k) > len(npath) {
-			list := strings.Split(strings.Trim(k[len(npath):], "/"), "/")
-			name := list[0]
-			if _, ok := exists[name]; !ok {
-				exists[name] = name
-				paths = append(paths, name)
-			}
-
-		}
-	}
-	return paths, 0, nil
-}
-
-func (l *localMemory) WatchValue(path string) (data chan registry.ValueWatcher, err error) {
-	v := &eventWatcher{
-		watcher: make(chan registry.ValueWatcher),
-	}
-
-	return v.watcher, nil
-
-}
-func (l *localMemory) WatchChildren(path string) (data chan registry.ChildrenWatcher, err error) {
-	return nil, nil
-}
-func (l *localMemory) Delete(path string) error {
-	l.lock.Lock()
-	defer l.lock.Unlock()
-	np := r.Join(path)
-	for k := range l.nodes {
-		if strings.HasPrefix(k, np) {
-			delete(l.nodes, k)
-		}
-	}
-	return nil
-}
-
-func (l *localMemory) CreatePersistentNode(path string, data string) (err error) {
-	l.lock.RLock()
-	defer l.lock.RUnlock()
-	list := strings.Split(r.Join(path), "/")
-	for i := range list {
-		path := r.Join(list[:i]...)
-		if _, ok := l.nodes[path]; !ok {
-			l.nodes[path] = "{}"
-		}
-	}
-	l.nodes[r.Join(path)] = data
-	return nil
-}
-func (l *localMemory) CreateTempNode(path string, data string) (err error) {
-	return l.CreatePersistentNode(path, data)
-}
-func (l *localMemory) CreateSeqNode(path string, data string) (rpath string, err error) {
-	nid := atomic.AddInt32(&l.seqValue, 1)
-	rpath = fmt.Sprintf("%s%d", path, nid)
-	err = l.CreatePersistentNode(rpath, data)
-	return
-}
-
 func (l *localMemory) Close() error {
 	return nil
-}
-
-type valueEntity struct {
-	Value   []byte
-	version int32
-	path    string
-	Err     error
-}
-type valuesEntity struct {
-	values  []string
-	version int32
-	path    string
-	Err     error
-}
-
-func (v *valueEntity) GetPath() string {
-	return v.path
-}
-func (v *valueEntity) GetValue() ([]byte, int32) {
-	return v.Value, v.version
-}
-func (v *valueEntity) GetError() error {
-	return v.Err
-}
-
-func (v *valuesEntity) GetValue() ([]string, int32) {
-	return v.values, v.version
-}
-func (v *valuesEntity) GetError() error {
-	return v.Err
-}
-func (v *valuesEntity) GetPath() string {
-	return v.path
-}
-
-type eventWatcher struct {
-	watcher chan registry.ValueWatcher
 }
 
 //zkRegistry 基于zookeeper的注册中心
