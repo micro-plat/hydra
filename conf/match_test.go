d6b0d7e4bdd04a379232217cff93af3e4092f594
liujinyin <liujy@100bm.cn>
2020-10-15 17:28:12 +0800

    1. 修改match 的bug 2. 修改middleware mock context 的文件名

--- conf/match_test.go
+++ conf/match_test.go
@@ -1,10 +1,7 @@
 package conf
 
 import (
-	"sort"
 	"testing"
-
-	"github.com/micro-plat/lib4go/concurrent/cmap"
 )
 
 func Test_sortString_Len(t *testing.T) {
@@ -58,7 +55,7 @@ func Test_sortString_Swap(t *testing.T) {
 		{
 			name: "t1",
 			s:    []string{},
-			args: args{i: 0, j: 1},
+			args: args{i: 0, j: 0},
 		},
 		{
 			name: "t2",
@@ -81,6 +78,9 @@ func Test_sortString_Swap(t *testing.T) {
 			if len(tt.s) < tt.args.i || len(tt.s) < tt.args.j {
 				t.Errorf("用例[%s]out of range", tt.name)
 			} else {
+				if len(tt.s) == 0 {
+					return
+				}
 				oi := tt.s[tt.args.i]
 				oj := tt.s[tt.args.j]
 				tt.s.Swap(tt.args.i, tt.args.j)
@@ -148,7 +148,7 @@ func Test_sortString_Less(t *testing.T) {
 			name: "t9", //j字符串被i包含
 			s:    sortString{"/t1/t2/t3", "/t1/t2"},
 			args: args{i: 0, j: 1},
-			want: true,
+			want: false,
 		}, {
 			name: "t10", //ip中.的判断
 			s:    sortString{"192.168.*.*", "192.168.5.94"},
@@ -182,8 +182,7 @@ func Test_sortString_Less(t *testing.T) {
 
 func TestPathMatch_Match(t *testing.T) {
 	type fields struct {
-		cache cmap.ConcurrentMap
-		all   []string
+		all []string
 	}
 	type args struct {
 		service string
@@ -198,127 +197,127 @@ func TestPathMatch_Match(t *testing.T) {
 		// TODO: Add test cases.
 		{
 			name:   "t1",
-			fields: fields{cache: cmap.New(6), all: []string{"/test1"}},
+			fields: fields{all: []string{"/test1"}},
 			args:   args{service: "/"},
 			want:   false,
 			want1:  "",
 		}, {
 			name:   "t2",
-			fields: fields{cache: cmap.New(6), all: []string{"/", "/t1", "/t1/t2", "/t1/t2/t3", "/t1/t2/t3/t4", "/t1/t2/t3/t4/t5"}},
+			fields: fields{all: []string{"/", "/t1", "/t1/t2", "/t1/t2/t3", "/t1/t2/t3/t4", "/t1/t2/t3/t4/t5"}},
 			args:   args{service: "/"},
 			want:   true,
 			want1:  "/",
 		}, {
 			name:   "t3",
-			fields: fields{cache: cmap.New(6), all: []string{"/", "/t1", "/t1/t2", "/t1/t2/t3", "/t1/t2/t3/t4", "/t1/t2/t3/t4/t5"}},
+			fields: fields{all: []string{"/", "/t1", "/t1/t2", "/t1/t2/t3", "/t1/t2/t3/t4", "/t1/t2/t3/t4/t5"}},
 			args:   args{service: "/t2/t3"},
 			want:   false,
 			want1:  "",
 		}, {
 			name:   "t4",
-			fields: fields{cache: cmap.New(6), all: []string{"/", "/t1", "/t1/t2", "/t1/t2/t3", "/t1/t2/t3/t4", "/t1/t2/t3/t4/t5"}},
+			fields: fields{all: []string{"/", "/t1", "/t1/t2", "/t1/t2/t3", "/t1/t2/t3/t4", "/t1/t2/t3/t4/t5"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/t1/t2/t3",
 		}, {
 			name:   "t5",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/*"}},
+			fields: fields{all: []string{"/t1/*"}},
 			args:   args{service: "/t1/t2"},
 			want:   true,
 			want1:  "/t1/*",
 		}, {
 			name:   "t6",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/*"}},
+			fields: fields{all: []string{"/t1/*"}},
 			args:   args{service: "/t1/t3"},
 			want:   true,
 			want1:  "/t1/*",
 		}, {
 			name:   "t7",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/*", "/t1/*/*"}},
+			fields: fields{all: []string{"/t1/*", "/t1/*/*"}},
 			args:   args{service: "/t1/t3/dd"},
 			want:   true,
 			want1:  "/t1/*/*",
 		}, {
 			name:   "t8",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/*", "/t1/t2/*"}},
+			fields: fields{all: []string{"/t1/*", "/t1/t2/*"}},
 			args:   args{service: "/t1/t2/dd"},
 			want:   true,
 			want1:  "/t1/t2/*",
 		}, {
 			name:   "t9",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/ss", "/t1/t2/*"}},
+			fields: fields{all: []string{"/t1/t2/ss", "/t1/t2/*"}},
 			args:   args{service: "/t1/t2/dd"},
 			want:   true,
 			want1:  "/t1/t2/*",
 		}, {
 			name:   "t10",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/ss", "/t1/t2/*"}},
+			fields: fields{all: []string{"/t1/t2/ss", "/t1/t2/*"}},
 			args:   args{service: "/t1/t2/ss"},
 			want:   true,
 			want1:  "/t1/t2/ss",
 		}, {
 			name:   "t11",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/:name"}},
+			fields: fields{all: []string{"/t1/t2/:name"}},
 			args:   args{service: "/t1/t2/ss"},
 			want:   false,
 			want1:  "",
 		}, {
 			name:   "t12",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/t3", "/t1/*/t4"}},
+			fields: fields{all: []string{"/t1/t2/t3", "/t1/*/t4"}},
 			args:   args{service: "/t1/t2/t4"},
 			want:   true,
 			want1:  "/t1/*/t4",
 		}, {
 			name:   "t13",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/t3", "/t1/*/t4"}},
+			fields: fields{all: []string{"/t1/t2/t3", "/t1/*/t4"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/t1/t2/t3",
 		}, {
 			name:   "t14",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/**"}},
+			fields: fields{all: []string{"/t1/**"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/t1/**",
 		}, {
 			name:   "t15",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/**", "/t1/*/t4"}},
+			fields: fields{all: []string{"/t1/**", "/t1/*/t4"}},
 			args:   args{service: "/t1/t2/t4"},
 			want:   true,
 			want1:  "/t1/*/t4",
 		}, {
 			name:   "t16",
-			fields: fields{cache: cmap.New(6), all: []string{"192.168.5.124", "192.168.5.22"}},
+			fields: fields{all: []string{"192.168.5.124", "192.168.5.22"}},
 			args:   args{service: "192.168.5.94"},
 			want:   false,
 			want1:  "",
 		}, {
 			name:   "t17",
-			fields: fields{cache: cmap.New(6), all: []string{"192.168.5.124", "192.168.5.94"}},
+			fields: fields{all: []string{"192.168.5.124", "192.168.5.94"}},
 			args:   args{service: "192.168.5.94"},
 			want:   true,
 			want1:  "192.168.5.94",
 		}, {
 			name:   "t18",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/t3", "/t1/t2/*", "/t1/*/t2", "/t1/**"}},
+			fields: fields{all: []string{"/t1/t2/t3", "/t1/t2/*", "/t1/*/t2", "/t1/**"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/t1/t2/t3",
 		}, {
 			name:   "t19",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/t2/*", "/t1/*/t2", "/t1/**"}},
+			fields: fields{all: []string{"/t1/t2/*", "/t1/*/t2", "/t1/**"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/t1/t2/*",
 		}, {
 			name:   "t20",
-			fields: fields{cache: cmap.New(6), all: []string{"/t1/*/t3", "/t1/**"}},
+			fields: fields{all: []string{"/t1/*/t3", "/t1/**"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/t1/*/t3",
 		}, {
 			name:   "t21",
-			fields: fields{cache: cmap.New(6), all: []string{"/**"}},
+			fields: fields{all: []string{"/**"}},
 			args:   args{service: "/t1/t2/t3"},
 			want:   true,
 			want1:  "/**",
@@ -326,11 +325,7 @@ func TestPathMatch_Match(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a := &PathMatch{
-				cache: tt.fields.cache,
-				all:   tt.fields.all,
-			}
-			sort.Sort(sortString(a.all))
+			a := NewPathMatch(tt.fields.all...)
 			got, got1 := a.Match(tt.args.service)
 			if got != tt.want {
 				t.Errorf("PathMatch.Match() got = %v, want %v", got, tt.want)
