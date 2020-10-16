8f6c4157bb357edcfbbe240abcf49462ab6a0c2b
hj <546921722@qq.com>
2020-10-16 18:43:35 +0800

    测试用例

--- test/context/ctx.path_test.go
+++ test/context/ctx.path_test.go
@@ -1,122 +1,136 @@
 package context
 
-// func Test_rpath_GetRouter(t *testing.T) {
+import (
+	"net/http"
+	"reflect"
+	"testing"
 
-// 	confObj := mocks.NewConf() //构建对象
-// 	confObj.API(":8080")       //初始化参数
-// 	confObj.CRON(c.WithMasterSlave(), c.WithTrace())
-// 	confObj.Service.API.Add("/api", "/api", []string{"GET"})
-// 	httpConf := confObj.GetAPIConf()  //获取配置
-// 	cronConf := confObj.GetCronConf() //获取配置
+	"github.com/micro-plat/hydra/conf"
+	"github.com/micro-plat/hydra/conf/server"
+	c "github.com/micro-plat/hydra/conf/server/cron"
+	"github.com/micro-plat/hydra/conf/server/router"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/context/ctx"
+	"github.com/micro-plat/hydra/test/mocks"
+)
 
-// 	type fields struct {
-// 		ctx        context.IInnerContext
-// 		serverConf server.IServerConf
-// 		meta       conf.IMeta
-// 		isLimit    bool
-// 		fallback   bool
-// 	}
-// 	tests := []struct {
-// 		name      string
-// 		fields    fields
-// 		want      *router.Router
-// 		wantError string
-// 	}{
-// 		{name: "http正确路径和正确方法", fields: fields{ctx: &mocks.TestContxt{
-// 			Routerpath: "/api",
-// 			Method:     "GET",
-// 		}, serverConf: httpConf, meta: conf.NewMeta()}, want: &router.Router{
-// 			Path:    "/api",
-// 			Action:  []string{"GET"},
-// 			Service: "/api",
-// 		}},
-// 		{name: "http非正确路径和方法", fields: fields{ctx: &mocks.TestContxt{
-// 			Routerpath: "/api",
-// 			Method:     "DELETE",
-// 		}, serverConf: httpConf, meta: conf.NewMeta()}, wantError: "未找到与[/api][DELETE]匹配的路由"},
-// 		{name: "非http的路径和的方法", fields: fields{ctx: &mocks.TestContxt{
-// 			Routerpath: "/cron",
-// 		}, serverConf: cronConf, meta: conf.NewMeta()}, want: &router.Router{
-// 			Path:     "/cron",
-// 			Encoding: "utf-8",
-// 			Action:   []string{},
-// 			Service:  "/cron",
-// 		}},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			c := ctx.NewRpath(tt.fields.ctx, tt.fields.serverConf, tt.fields.meta)
-// 			defer func() {
-// 				if r := recover(); r != nil {
-// 					if tt.wantError == r {
-// 						return
-// 					}
-// 					t.Errorf("rpath.GetRouter() err %+v", r)
-// 				}
-// 			}()
-// 			if got := c.GetRouter(); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("rpath.GetRouter() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func Test_rpath_GetRouter(t *testing.T) {
 
-// func Test_rpath_GetCookies(t *testing.T) {
+	confObj := mocks.NewConf() //构建对象
+	confObj.API(":8080")       //初始化参数
+	confObj.CRON(c.WithMasterSlave(), c.WithTrace())
+	confObj.Service.API.Add("/api", "/api", []string{"GET"})
+	httpConf := confObj.GetAPIConf()  //获取配置
+	cronConf := confObj.GetCronConf() //获取配置
 
-// 	confObj := mocks.NewConf()         //构建对象
-// 	confObj.API(":8080")               //初始化参数
-// 	serverConf := confObj.GetAPIConf() //获取配置
-// 	type fields struct {
-// 		ctx context.IInnerContext
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		fields fields
-// 		want   map[string]string
-// 	}{
-// 		{name: "获取全部cookies", fields: fields{ctx: &mocks.TestContxt{
-// 			Cookie: []*http.Cookie{&http.Cookie{Name: "cookie1", Value: "value1"}, &http.Cookie{Name: "cookie2", Value: "value2"}},
-// 		}}, want: map[string]string{"cookie1": "value1", "cookie2": "value2"}},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			c := ctx.NewRpath(tt.fields.ctx, serverConf, conf.NewMeta())
-// 			if got := c.GetCookies(); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("rpath.GetCookies() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+	type fields struct {
+		ctx        context.IInnerContext
+		serverConf server.IServerConf
+		meta       conf.IMeta
+		isLimit    bool
+		fallback   bool
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		want      *router.Router
+		wantError string
+	}{
+		{name: "http正确路径和正确方法", fields: fields{ctx: &mocks.TestContxt{
+			Routerpath: "/api",
+			Method:     "GET",
+		}, serverConf: httpConf, meta: conf.NewMeta()}, want: &router.Router{
+			Path:    "/api",
+			Action:  []string{"GET"},
+			Service: "/api",
+		}},
+		{name: "http非正确路径和方法", fields: fields{ctx: &mocks.TestContxt{
+			Routerpath: "/api",
+			Method:     "DELETE",
+		}, serverConf: httpConf, meta: conf.NewMeta()}, wantError: "未找到与[/api][DELETE]匹配的路由"},
+		{name: "非http的路径和的方法", fields: fields{ctx: &mocks.TestContxt{
+			Routerpath: "/cron",
+		}, serverConf: cronConf, meta: conf.NewMeta()}, want: &router.Router{
+			Path:     "/cron",
+			Encoding: "utf-8",
+			Action:   []string{},
+			Service:  "/cron",
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := ctx.NewRpath(tt.fields.ctx, tt.fields.serverConf, tt.fields.meta)
+			defer func() {
+				if r := recover(); r != nil {
+					if tt.wantError == r {
+						return
+					}
+					t.Errorf("rpath.GetRouter() err %+v", r)
+				}
+			}()
+			if got := c.GetRouter(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rpath.GetRouter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-// func Test_rpath_GetCookie(t *testing.T) {
-// 	confObj := mocks.NewConf()         //构建对象
-// 	confObj.API(":8080")               //初始化参数
-// 	serverConf := confObj.GetAPIConf() //获取配置
-// 	rpath := ctx.NewRpath(&mocks.TestContxt{
-// 		Cookie: []*http.Cookie{&http.Cookie{Name: "cookie1", Value: "value1"}, &http.Cookie{Name: "cookie2", Value: "value2"}},
-// 	}, serverConf, conf.NewMeta())
+func Test_rpath_GetCookies(t *testing.T) {
 
-// 	type args struct {
-// 		name string
-// 	}
-// 	tests := []struct {
-// 		name  string
-// 		args  args
-// 		want  string
-// 		want1 bool
-// 	}{
-// 		{name: "获取存在cookies", args: args{name: "cookie2"}, want: "value2", want1: true},
-// 		{name: "获取不存在cookies", args: args{name: "cookie3"}, want: "", want1: false},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got, got1 := rpath.GetCookie(tt.args.name)
-// 			if got != tt.want {
-// 				t.Errorf("rpath.GetCookie() got = %v, want %v", got, tt.want)
-// 			}
-// 			if got1 != tt.want1 {
-// 				t.Errorf("rpath.GetCookie() got1 = %v, want %v", got1, tt.want1)
-// 			}
-// 		})
-// 	}
-// }
+	confObj := mocks.NewConf()         //构建对象
+	confObj.API(":8080")               //初始化参数
+	serverConf := confObj.GetAPIConf() //获取配置
+	type fields struct {
+		ctx context.IInnerContext
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   map[string]string
+	}{
+		{name: "获取全部cookies", fields: fields{ctx: &mocks.TestContxt{
+			Cookie: []*http.Cookie{&http.Cookie{Name: "cookie1", Value: "value1"}, &http.Cookie{Name: "cookie2", Value: "value2"}},
+		}}, want: map[string]string{"cookie1": "value1", "cookie2": "value2"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := ctx.NewRpath(tt.fields.ctx, serverConf, conf.NewMeta())
+			if got := c.GetCookies(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rpath.GetCookies() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_rpath_GetCookie(t *testing.T) {
+	confObj := mocks.NewConf()         //构建对象
+	confObj.API(":8080")               //初始化参数
+	serverConf := confObj.GetAPIConf() //获取配置
+	rpath := ctx.NewRpath(&mocks.TestContxt{
+		Cookie: []*http.Cookie{&http.Cookie{Name: "cookie1", Value: "value1"}, &http.Cookie{Name: "cookie2", Value: "value2"}},
+	}, serverConf, conf.NewMeta())
+
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 bool
+	}{
+		{name: "获取存在cookies", args: args{name: "cookie2"}, want: "value2", want1: true},
+		{name: "获取不存在cookies", args: args{name: "cookie3"}, want: "", want1: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := rpath.GetCookie(tt.args.name)
+			if got != tt.want {
+				t.Errorf("rpath.GetCookie() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("rpath.GetCookie() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
---
+++ test/registry/registry_test.go
@@ -0,0 +1,162 @@
+package registry
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/micro-plat/hydra/context/ctx"
+	"github.com/micro-plat/hydra/registry"
+	_ "github.com/micro-plat/hydra/registry/registry/etcd"
+	_ "github.com/micro-plat/hydra/registry/registry/filesystem"
+	_ "github.com/micro-plat/hydra/registry/registry/localmemory"
+	_ "github.com/micro-plat/hydra/registry/registry/redis"
+	_ "github.com/micro-plat/hydra/registry/registry/zookeeper"
+	"github.com/micro-plat/lib4go/logger"
+
+	"github.com/micro-plat/hydra/conf"
+	"github.com/micro-plat/hydra/test/mocks"
+)
+
+// 私有变量的测试
+// func Test_getAddrByUserPass(t *testing.T) {
+// 	type args struct {
+// 		addr string
+// 	}
+// 	tests := []struct {
+// 		name        string
+// 		args        args
+// 		wantU       string
+// 		wantP       string
+// 		wantAddress string
+// 		wantErr     bool
+// 	}{
+// 		{name: "正确格式的地址", args: args{addr: "root:123456@192.168.5.115:9091"}, wantU: "root", wantP: "123456", wantAddress: "192.168.5.115:9091", wantErr: false},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			gotU, gotP, gotAddress, err := getAddrByUserPass(tt.args.addr)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("getAddrByUserPass() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if gotU != tt.wantU {
+// 				t.Errorf("getAddrByUserPass() gotU = %v, want %v", gotU, tt.wantU)
+// 			}
+// 			if gotP != tt.wantP {
+// 				t.Errorf("getAddrByUserPass() gotP = %v, want %v", gotP, tt.wantP)
+// 			}
+// 			if gotAddress != tt.wantAddress {
+// 				t.Errorf("getAddrByUserPass() gotAddress = %v, want %v", gotAddress, tt.wantAddress)
+// 			}
+// 		})
+// 	}
+// }
+
+func TestParse(t *testing.T) {
+	type args struct {
+		address string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantProto string
+		wantRaddr []string
+		wantU     string
+		wantP     string
+		wantErr   bool
+	}{
+		{name: "解析zk地址", args: args{address: "zk://192.168.0.101"}, wantProto: "zk", wantRaddr: []string{"192.168.0.101"}, wantU: "", wantP: "", wantErr: false},
+		{name: "解析多个zk地址", args: args{address: "zk://192.168.0.101,192.168.0.102"}, wantProto: "zk",
+			wantRaddr: []string{"192.168.0.101", "192.168.0.102"}, wantU: "", wantP: "", wantErr: false},
+		{name: "解析lm地址", args: args{address: "lm://."}, wantProto: "lm", wantRaddr: []string{"."}, wantU: "", wantP: "", wantErr: false},
+		{name: "解析fs地址", args: args{address: "fs://../a/b/c"}, wantProto: "fs", wantRaddr: []string{"../a/b/c"}, wantU: "", wantP: "", wantErr: false},
+		{name: "解析etcd地址", args: args{address: "etcd://192.168.0.101:9099"}, wantProto: "etcd", wantRaddr: []string{"192.168.0.101:9099"}, wantU: "", wantP: "", wantErr: false},
+		{name: "解析redis地址", args: args{address: "redis://192.168.0.101:6379"}, wantProto: "redis", wantRaddr: []string{"192.168.0.101:6379"}, wantU: "", wantP: "", wantErr: false},
+		{name: "解析带有用户名和密码的地址", args: args{address: "redis://root:123456@192.168.0.101:6379"}, wantProto: "redis",
+			wantRaddr: []string{"192.168.0.101:6379"}, wantU: "root", wantP: "123456", wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotProto, gotRaddr, gotU, gotP, err := registry.Parse(tt.args.address)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotProto != tt.wantProto {
+				t.Errorf("Parse() gotProto = %v, want %v", gotProto, tt.wantProto)
+			}
+			if !reflect.DeepEqual(gotRaddr, tt.wantRaddr) {
+				t.Errorf("Parse() gotRaddr = %v, want %v", gotRaddr, tt.wantRaddr)
+			}
+			if gotU != tt.wantU {
+				t.Errorf("Parse() gotU = %v, want %v", gotU, tt.wantU)
+			}
+			if gotP != tt.wantP {
+				t.Errorf("Parse() gotP = %v, want %v", gotP, tt.wantP)
+			}
+		})
+	}
+}
+
+func TestJoin(t *testing.T) {
+	type args struct {
+		elem []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "地址拼接", args: args{elem: []string{"", ""}}, want: ""},
+		{name: "地址拼接", args: args{elem: []string{"", "a/"}}, want: "/a"},
+		{name: "地址拼接", args: args{elem: []string{"a", "b", "c"}}, want: "/a/b/c"},
+		{name: "地址拼接", args: args{elem: []string{"..", "a/b", "c/"}}, want: "/../a/b/c"},
+		{name: "地址拼接", args: args{elem: []string{"..", "", "\\", "c/"}}, want: "/../c"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := registry.Join(tt.args.elem...); got != tt.want {
+				t.Errorf("Join() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewRegistry(t *testing.T) {
+	confObj := mocks.NewConf()         //构建对象
+	confObj.API(":8080")               //初始化参数
+	serverConf := confObj.GetAPIConf() //获取配置
+	meta := conf.NewMeta()
+	log := logger.GetSession(serverConf.GetMainConf().GetServerName(), ctx.NewUser(&mocks.TestContxt{}, meta).GetRequestID())
+	type args struct {
+		address string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantR   registry.IRegistry
+		wantErr bool
+		err     string
+	}{
+		{name: "获取zk的注册中心", args: args{address: "zk://192.168.0.101"}, wantErr: false},
+		{name: "获取lm的注册中心", args: args{address: "lm://."}, wantErr: false},
+		{name: "获取fs的注册中心", args: args{address: "fs://../"}, wantErr: true, err: "配置文件不存在:../registry.test.conf.toml stat ../registry.test.conf.toml: no such file or directory"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			gotR, err := registry.NewRegistry(tt.args.address, log)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewRegistry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err != nil && tt.wantErr && err.Error() != tt.err {
+				t.Errorf("NewRegistry() error = %v, wantErr %v", err.Error(), tt.err)
+				return
+			}
+			if !reflect.DeepEqual(gotR, tt.wantR) {
+				t.Errorf("NewRegistry() = %v, want %v", gotR, tt.wantR)
+			}
+		})
+	}
+}
