406fb02ddf24a0143353c58a6728ffa00e5774f9
taosy <taosy@100bm.cn>
2020-10-20 14:49:17 +0800

    上传测试代码

---
+++ tests/apiserver/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/micro-plat/hydra/conf/server/acl/blacklist"
+	"github.com/micro-plat/hydra/conf/server/api"
+	ccron "github.com/micro-plat/hydra/conf/server/cron"
+	crpc "github.com/micro-plat/hydra/conf/server/rpc"
+	"github.com/micro-plat/hydra/conf/server/task"
+
+	"github.com/micro-plat/hydra"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/hydra/servers/cron"
+	"github.com/micro-plat/hydra/hydra/servers/http"
+	"github.com/micro-plat/hydra/hydra/servers/rpc"
+)
+
+var app = hydra.NewApp(
+	hydra.WithServerTypes(http.API, rpc.RPC, cron.CRON),
+	hydra.WithPlatName("taosytest"),
+	hydra.WithSystemName("test-confcache"),
+	hydra.WithClusterName("taosy"),
+	hydra.WithRegistry("zk://192.168.0.101"),
+)
+
+func init() {
+	hydra.Conf.API(":8070", api.WithTimeout(10, 10)).BlackList(blacklist.WithEnable(), blacklist.WithIP("192.168.0.101"))
+	hydra.Conf.RPC(":8888", crpc.WithTimeout(10, 10))
+	hydra.Conf.CRON(ccron.WithTimeout(10)).Task(task.NewTask("@ervery 10s", "/taosy/testcron"))
+	app.API("/taosy/testapi", func(ctx context.IContext) (r interface{}) {
+		ctx.Log().Info("api 接口服务测试")
+		return "success"
+	})
+	app.RPC("/taosy/testrpc", func(ctx context.IContext) (r interface{}) {
+		ctx.Log().Info("rpc 接口服务测试")
+		return "success"
+	})
+	app.CRON("/taosy/testcron", func(ctx context.IContext) (r interface{}) {
+		ctx.Log().Info("cron 接口服务测试")
+		return "success"
+	})
+}
+
+func main() {
+	app.Start()
+}
