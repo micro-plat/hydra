2bacfdfc13ca57c77737264c6f9b7a92fce2a0be
zkfy <lib4net@163.com>
2020-11-05 14:44:04 +0800

    优化函数命名，添加注释

--- test/conf/server.server_cache_test.go
+++ test/conf/server.server_cache_test.go
@@ -31,7 +31,7 @@ func Test_cache_GetServerConf(t *testing.T) {
 	assert.Equal(t, true, (err != nil), "获取rpc的配置对象，err")
 	assert.Equal(t, nil, obj, "获取rpc的配置对象，res")
 
-	serverMap := cacheobjPrt.GetServerMaps()
+	serverMap := cacheobjPrt.GetServerHistory()
 	key := fmt.Sprintf("%s-%v", "api", serveC.GetServerConf().GetVersion())
 	serverMap.Remove(key)
 	obj, err = cacheobjPrt.GetAPPConf("api")
@@ -43,7 +43,7 @@ func Test_cache_GetVarConf(t *testing.T) {
 	//复制一个空的chache对象
 	cacheobj := *app.Cache
 	cacheobjPrt := &cacheobj
-	app.Cache.GetVarMaps().Clear()
+	app.Cache.GetVarHistory().Clear()
 	//在空的缓存对象获取配置
 	obj, err := cacheobjPrt.GetVarConf()
 	assert.Equal(t, true, (err != nil), "获取不存在的配置对象，err")
@@ -57,21 +57,21 @@ func Test_cache_Clear(t *testing.T) {
 	serveC := conf.GetAPIConf()
 	app.Cache.Save(serveC)
 	oldVerion := serveC.GetServerConf().GetVersion()
-	assert.Equal(t, serveC.GetServerConf().GetVersion(), app.Cache.GetServerCuurVerion("api"), "api serverconf verion Equl")
-	assert.Equal(t, oldVerion, app.Cache.GetServerCuurVerion("api"), "当前版本号比对")
+	assert.Equal(t, serveC.GetServerConf().GetVersion(), app.Cache.GetCurrentServerVerion("api"), "api serverconf verion Equl")
+	assert.Equal(t, oldVerion, app.Cache.GetCurrentServerVerion("api"), "当前版本号比对")
 
 	//在添加一个server配置
 	serveB := conf.GetAPIConf()
 	cuurtVerion := serveB.GetServerConf().GetVersion()
 	app.Cache.Save(serveB)
-	_, ok := app.Cache.GetServerMaps().Get(fmt.Sprintf("api-%v", oldVerion))
+	_, ok := app.Cache.GetServerHistory().Get(fmt.Sprintf("api-%v", oldVerion))
 	assert.Equal(t, ok, true, "清除前，旧配置存在")
-	assert.Equal(t, cuurtVerion, app.Cache.GetServerCuurVerion("api"), "清除前，当前版本号比对")
+	assert.Equal(t, cuurtVerion, app.Cache.GetCurrentServerVerion("api"), "清除前，当前版本号比对")
 
 	time.Sleep(51 * time.Second)
-	_, ok = app.Cache.GetServerMaps().Get(fmt.Sprintf("api-%v", oldVerion))
+	_, ok = app.Cache.GetServerHistory().Get(fmt.Sprintf("api-%v", oldVerion))
 	assert.Equal(t, ok, false, "清除后，旧配置不存在")
-	_, ok = app.Cache.GetServerMaps().Get(fmt.Sprintf("api-%v", cuurtVerion))
+	_, ok = app.Cache.GetServerHistory().Get(fmt.Sprintf("api-%v", cuurtVerion))
 	assert.Equal(t, ok, true, "清除后，新配置存在")
-	assert.Equal(t, cuurtVerion, app.Cache.GetServerCuurVerion("api"), "清除后，当前版本号比对")
+	assert.Equal(t, cuurtVerion, app.Cache.GetCurrentServerVerion("api"), "清除后，当前版本号比对")
 }
