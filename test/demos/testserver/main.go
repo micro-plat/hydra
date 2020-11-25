5514ca7059c09f98d6397d004b67140441388577
liujinyin <liujy@100bm.cn>
2020-11-25 16:42:47 +0800

    修改rpc 的调用 ，支持IP 方式的调用

--- test/demos/testserver/main.go
+++ test/demos/testserver/main.go
@@ -4,16 +4,16 @@ import (
 	"github.com/micro-plat/hydra"
 	"github.com/micro-plat/hydra/components"
 	_ "github.com/micro-plat/hydra/components/caches/cache/redis"
-	xrpc "github.com/micro-plat/hydra/components/rpcs/rpc"
 	"github.com/micro-plat/hydra/conf/server/task"
-	queueredis "github.com/micro-plat/hydra/conf/vars/queue/redis"
+	"github.com/micro-plat/hydra/conf/vars/queue/queueredis"
 	confRedis "github.com/micro-plat/hydra/conf/vars/redis"
 	"github.com/micro-plat/hydra/context"
 	"github.com/micro-plat/hydra/hydra/servers/cron"
+	"github.com/micro-plat/hydra/hydra/servers/rpc"
 )
 
 var app = hydra.NewApp(
-	hydra.WithServerTypes(cron.CRON),
+	hydra.WithServerTypes(cron.CRON, rpc.RPC),
 	hydra.WithPlatName("taosytest"),
 	hydra.WithSystemName("testserver"),
 	hydra.WithClusterName("t"),
@@ -25,6 +25,7 @@ func init() {
 	hydra.Conf.Vars().RPC("rpc")
 	hydra.Conf.Vars().Redis("redis", confRedis.New(nil, confRedis.WithAddrs("192.168.5.79:6379")))
 	hydra.Conf.Vars().Queue().Redis("redis", queueredis.New(queueredis.WithConfigName("redis")))
+	hydra.Conf.RPC(":50003")
 	hydra.Conf.CRON().Task(task.NewTask("@every 1s", "/taosy/testserver"))
 	app.CRON("/taosy/testserver", func(ctx context.IContext) (r interface{}) {
 		ctx.Log().Info("cron-hour 接口服务测试")
@@ -35,12 +36,13 @@ func init() {
 			return
 		}
 
-		rclient, err := xrpc.NewClient("http://192.168.0.137:8888", "", "")
+		request, err := hydra.C.RPC().GetRPC()
 		if err != nil {
-			ctx.Log().Error("获取rpc客户端异常", err)
+			ctx.Log().Error("获取GetRPC客户端异常", err)
 			return
 		}
-		for i := 0; i < 100; i++ {
+
+		for i := 0; i < 1; i++ {
 			// go func(httpObj http.IClient) {
 			content, status, err := httpObj.Get("http://192.168.0.137:8070/taosy/testapi")
 			if err != nil || status != 200 {
@@ -71,18 +73,35 @@ func init() {
 				ctx.Log().Errorf("发送queue2队列消息异常, err:%v", err)
 			}
 
-			resp, err := rclient.Request(ctx.Context(), "/testrpc/service1", map[string]interface{}{"rpc": "test"})
+			ctx.Log().Info("RPC.IP.Result-1:")
+			response, err := request.Request(ctx.Context(), "/testrpc/service1@tcp://192.168.0.137:8888", map[string]interface{}{"rpc": "test"})
 			if err != nil {
-				ctx.Log().Errorf("rpc请求异常, err:%v", err)
+				ctx.Log().Error("RPC.IP.Request异常", err)
+				return
 			}
-			if resp != nil {
-				ctx.Log().Infof("rpc请求结果, Status:%d,Result:%s", resp.Status, resp.Result)
-			}
+			ctx.Log().Info("RPC.IP.Result:", response.Result)
 
-			// }(httpObj)
+			ctx.Log().Info("RPC.PlatName.Result-1:")
+			response, err = request.Request(ctx.Context(), "/test/rpc@taosytest", map[string]interface{}{"rpc": "plattest"})
+			if err != nil {
+				ctx.Log().Error("RPC.PlatName.Request异常", err)
+				return
+			}
+			ctx.Log().Info("RPC.PlatName.Result:", response.Result)
+
+			ctx.Log().Info("RPC.OLD.Result-1:")
+			response, err = request.Request(ctx.Context(), "/test/rpc@taosytest", map[string]interface{}{"rpc": "plattest"})
+			if err != nil {
+				ctx.Log().Error("RPC.OLD.Request异常", err)
+				return
+			}
+			ctx.Log().Info("RPC.OLD.Result:", response.Result)
 		}
 
 		return
+	})
+	app.RPC("/test/rpc", func(ctx context.IContext) (r interface{}) {
+		return "test.rpc.success"
 	})
 }
 
