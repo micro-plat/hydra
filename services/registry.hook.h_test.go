78cc8c078772cf529ba7a270b821a007e7551be6
hj <546921722@qq.com>
2020-11-26 09:35:46 +0800

    测试修改

--- services/registry.hook.h_test.go
+++ services/registry.hook.h_test.go
@@ -15,10 +15,10 @@ func Test_handleHook_AddHandling(t *testing.T) {
 		wantErr     bool
 		wantHanding []context.IHandler
 	}{
-		{name: "添加空业务处理接口", service: "service", wantHanding: nil},
-		{name: "添加业务处理接口", service: "service", h: []context.IHandler{hander1{}}, wantHanding: []context.IHandler{hander1{}}},
-		{name: "再次添加业务处理接口", service: "service", h: []context.IHandler{hander2{}}, wantHanding: []context.IHandler{hander1{}, hander2{}}},
-		{name: "再次添加同样业务处理接口", service: "service", h: []context.IHandler{hander2{}}, wantHanding: []context.IHandler{hander1{}, hander2{}, hander2{}}},
+		{name: "1.1 添加空接口", service: "service", wantHanding: nil},
+		{name: "1.2 添加非空接口", service: "service", h: []context.IHandler{hander1{}}, wantHanding: []context.IHandler{hander1{}}},
+		{name: "1.3 添加不存在接口", service: "service", h: []context.IHandler{hander2{}}, wantHanding: []context.IHandler{hander1{}, hander2{}}},
+		{name: "1.4 添加已存在理接口", service: "service", h: []context.IHandler{hander2{}}, wantHanding: []context.IHandler{hander1{}, hander2{}, hander2{}}},
 	}
 	s := newHandleHook()
 	for _, tt := range tests {
@@ -37,10 +37,10 @@ func Test_handleHook_AddHandled(t *testing.T) {
 		wantErr     bool
 		wantHandled []context.IHandler
 	}{
-		{name: "添加空业务处理接口", service: "service", wantHandled: nil},
-		{name: "添加业务处理接口", service: "service", h: []context.IHandler{hander1{}}, wantHandled: []context.IHandler{hander1{}}},
-		{name: "再次添加业务处理接口", service: "service", h: []context.IHandler{hander2{}}, wantHandled: []context.IHandler{hander1{}, hander2{}}},
-		{name: "再次添加同样业务处理接口", service: "service", h: []context.IHandler{hander2{}}, wantHandled: []context.IHandler{hander1{}, hander2{}, hander2{}}},
+		{name: "1.1 添加空接口", service: "service", wantHandled: nil},
+		{name: "1.2 添加非空接口", service: "service", h: []context.IHandler{hander1{}}, wantHandled: []context.IHandler{hander1{}}},
+		{name: "1.3 添加不存在接口", service: "service", h: []context.IHandler{hander2{}}, wantHandled: []context.IHandler{hander1{}, hander2{}}},
+		{name: "1.4 添加已存在理接口", service: "service", h: []context.IHandler{hander2{}}, wantHandled: []context.IHandler{hander1{}, hander2{}, hander2{}}},
 	}
 	s := newHandleHook()
 	for _, tt := range tests {
@@ -60,10 +60,10 @@ func Test_handleHook_AddClosingHanle(t *testing.T) {
 		wantErr        bool
 		wantClosingLen int
 	}{
-		{name: "添加空钩子"},
-		{name: "添加不支持类型的钩子", c: []interface{}{"123456"}, wantErr: true},
-		{name: "添加单个钩子", c: []interface{}{f1}, wantClosingLen: 1},
-		{name: "再次添加多个钩子", c: []interface{}{f1, nil, f2}, wantClosingLen: 3},
+		{name: "1.1 添加空钩子"},
+		{name: "1.2 添加不支持类型的钩子", c: []interface{}{"123456"}, wantErr: true},
+		{name: "1.3 添加钩子", c: []interface{}{f1}, wantClosingLen: 1},
+		{name: "1.4 添加多个钩子", c: []interface{}{f1, nil, f2}, wantClosingLen: 3},
 	}
 	s := newHandleHook()
 	for _, tt := range tests {
