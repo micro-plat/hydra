d9a0a6c1b3e6ff7b6f6dc519f63f4465b5125dd5
liujinyin <liujy@100bm.cn>
2020-11-26 17:33:58 +0800

    1. 修改fs 注册中心的通知问题 2. 修改cluster 相关bug 3. 修改import 引用目录问题

--- registry/registry/filesystem/file.system.go
+++ registry/registry/filesystem/file.system.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
-	"sync/atomic"
+	"time"
 
 	//"github.com/micro-plat/hydra/global/compatible"
 
@@ -22,8 +22,9 @@ var fileMode = os.FileMode(0664)
 var dirMode = os.FileMode(0755)
 
 type eventWatcher struct {
-	watcher chan registry.ValueWatcher
-	event   chan fsnotify.Event
+	watcher  chan registry.ValueWatcher
+	event    chan fsnotify.Event
+	syncChan chan string
 }
 
 type fs struct {
@@ -32,7 +33,6 @@ type fs struct {
 	watchLock    sync.Mutex
 	tempNodes    map[string]bool
 	tempNodeLock sync.Mutex
-	seqNode      int32
 	closeCh      chan struct{}
 	rootDir      string
 	done         bool
@@ -40,9 +40,6 @@ type fs struct {
 
 //NewFileSystem 文件系统的注册中心
 func NewFileSystem(rootDir string) (*fs, error) {
-	// if err := compatible.CheckPrivileges(); err != nil {
-	// 	return nil, err
-	// }
 	w, err := fsnotify.NewWatcher()
 	if err != nil {
 		return nil, err
@@ -52,7 +49,6 @@ func NewFileSystem(rootDir string) (*fs, error) {
 		watcher:     w,
 		watcherMaps: make(map[string]*eventWatcher),
 		tempNodes:   make(map[string]bool),
-		seqNode:     10000,
 		closeCh:     make(chan struct{}),
 	}, nil
 }
@@ -68,7 +64,6 @@ func (l *fs) Start() {
 				if l.done {
 					return
 				}
-				fmt.Println("l.watcher.Events:", event)
 				func(event fsnotify.Event) {
 					l.watchLock.Lock()
 					defer l.watchLock.Unlock()
@@ -106,17 +101,6 @@ func (l *fs) getDataPath(path string) string {
 func (l *fs) Exists(path string) (bool, error) {
 	_, err := os.Stat(l.formatPath(path))
 	return err == nil || os.IsExist(err), nil
-}
-
-func (l *fs) getPaths(path string) []string {
-	nodes := strings.Split(strings.Trim(path, "/"), "/")
-	len := len(nodes)
-	paths := make([]string, 0, len)
-	for i := 0; i < len; i++ {
-		npath := "/" + strings.Join(nodes[:i+1], "/")
-		paths = append(paths, npath)
-	}
-	return paths
 }
 
 func (l *fs) GetValue(path string) (data []byte, version int32, err error) {
@@ -182,40 +166,57 @@ func (l *fs) WatchValue(path string) (data chan registry.ValueWatcher, err error
 		return v.watcher, nil
 	}
 	l.watcherMaps[realPath] = &eventWatcher{
-		event:   make(chan fsnotify.Event),
-		watcher: make(chan registry.ValueWatcher),
+		event:    make(chan fsnotify.Event),
+		watcher:  make(chan registry.ValueWatcher),
+		syncChan: make(chan string, 100),
 	}
 	go func(rpath string, v *eventWatcher) {
 		dataFile := l.getDataPath(rpath)
 		if err := l.watcher.Add(dataFile); err != nil {
 			v.watcher <- &valueEntity{path: rpath, Err: err}
 		}
-		select {
-		case <-l.closeCh:
-			return
-		case event := <-v.event:
-			if event.Op == fsnotify.Chmod {
-				return
-			}
+		go func(evtw *eventWatcher) {
+			ticker := time.NewTicker(time.Second * 2)
+			for {
+				select {
+				case <-ticker.C:
+					path := ""
+				INFOR:
+					for {
+						select {
+						case p := <-evtw.syncChan:
+							path = p
+						default:
+							break INFOR
 
-			buff, version, err := l.GetValue(rpath)
-			ett := &valueEntity{
-				path: rpath,
+						}
+					}
+					if len(path) > 0 {
+						ett := &valueEntity{
+							path: rpath,
+						}
+						evtw.watcher <- ett
+					}
+				}
 			}
-			if len(buff) == 0 {
-				ett.Err = fmt.Errorf("文件发生变化:%v", event.Op)
-			} else {
-				ett.Value = buff
-				ett.version = version
-				ett.Err = err
+		}(v)
+
+		for {
+			select {
+			case <-l.closeCh:
+				return
+			case event := <-v.event:
+				if event.Op == fsnotify.Chmod || event.Op == fsnotify.Rename {
+					break
+				}
+				v.syncChan <- rpath
 			}
-			fmt.Println("GetValue:", path, string(buff), version, err)
-			v.watcher <- ett
 		}
 	}(realPath, l.watcherMaps[realPath])
 
 	return l.watcherMaps[realPath].watcher, nil
 }
+
 func (l *fs) WatchChildren(path string) (data chan registry.ChildrenWatcher, err error) {
 	return nil, nil
 }
@@ -261,17 +262,13 @@ func (l *fs) CreateTempNode(path string, data string) (err error) {
 	return nil
 }
 func (l *fs) CreateSeqNode(path string, data string) (rpath string, err error) {
-	nid := atomic.AddInt32(&l.seqNode, 1)
+	nid := time.Now().UnixNano()
 	rpath = fmt.Sprintf("%s_%d", path, nid)
 	return rpath, l.CreateTempNode(rpath, data)
 }
 
 func (l *fs) GetSeparator() string {
 	return string(filepath.Separator)
-}
-
-func (l *fs) CanWirteDataInDir() bool {
-	return false
 }
 
 func (l *fs) Close() error {
