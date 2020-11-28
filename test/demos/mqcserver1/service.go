c990be993e3fbe6e1eafca023b18c8c32f5ae418
liujinyin <liujy@100bm.cn>
2020-11-28 13:07:49 +0800

    增加mqc demo 程序

---
+++ test/demos/mqcserver1/service.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/micro-plat/hydra/context"
+
+//----------
+type objService struct {
+}
+
+func (s *objService) Handle(ctx context.IContext) interface{} {
+	return "ptr.success"
+}
+
+func FuncService(ctx context.IContext) interface{} {
+	return "func.success"
+}
+
+func NewObjNoneError() (*objService, error) {
+	return &objService{}, nil
+}
+
+func NewObjWithError() *objService {
+	return &objService{}
+}
