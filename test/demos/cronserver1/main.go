bf0551d7c48359efe4bf2066709e0b2d997b4b90
liujinyin <liujy@100bm.cn>
2020-11-29 16:16:01 +0800

    添加mqc/cron demo

--- test/demos/cronserver1/main.go
+++ test/demos/cronserver1/main.go
@@ -5,18 +5,17 @@ import (
 	"time"
 
 	"github.com/micro-plat/hydra"
-	"github.com/micro-plat/hydra/conf/server/queue"
 	"github.com/micro-plat/hydra/conf/vars/queue/queueredis"
 	"github.com/micro-plat/hydra/conf/vars/redis"
 	"github.com/micro-plat/hydra/context"
-	"github.com/micro-plat/hydra/hydra/servers/http"
+	"github.com/micro-plat/hydra/hydra/servers/cron"
 	"github.com/micro-plat/hydra/hydra/servers/mqc"
 )
 
 var app = hydra.NewApp(
-	hydra.WithServerTypes(http.API, mqc.MQC),
+	hydra.WithServerTypes(cron.CRON, mqc.MQC),
 	hydra.WithPlatName("hydra-examples"),
-	hydra.WithSystemName("mqcserver01"),
+	hydra.WithSystemName("cronserver01"),
 	hydra.WithClusterName("t1"))
 
 func main() {
@@ -24,11 +23,26 @@ func main() {
 	const redisServer = "192.168.0.101"
 
 	hydra.Conf.Vars().Redis("0.101", redis.New(nil, redis.WithAddrs(redisServer)))
-	hydra.Conf.Vars().Queue().Redis("redisqueue", queueredis.New(queueredis.WithConfigName("0.101")))
-	hydra.Conf.MQC("redis://redisqueue").Queue(queue.NewQueue("service:queue1", "/test/mqc/proc1"))
+	hydra.Conf.Vars().Queue().Redis("mqcqueue", queueredis.New(queueredis.WithConfigName("0.101")))
+	hydra.Conf.MQC("redis://mqcqueue")
 	hydra.Conf.API(":59001")
 
-	app.MQC("/testmqc/proc1", &objService{}, "service:queue1")
+	app.CRON("/testcron/proc1", &cronService{}, "@every 10s")
+	app.CRON("/testcron/proc2", func(ctx context.IContext) interface{} {
+		fmt.Println("ONCE---")
+		return nil
+	}, "@once")
+	app.CRON("/testcron/proc3", func(ctx context.IContext) interface{} {
+		fmt.Println("Now")
+		return nil
+	}, "@now")
+
+	app.CRON("/testcron/proc4", func(ctx context.IContext) interface{} {
+		fmt.Println("time")
+		return nil
+	}, "0 12 * * ?")
+
+	app.MQC("/testmqc/proc1", &mqcService{}, "service:queue1")
 	//app.MQC("/testmqc/proc2", objService{}, "service:queue2")
 	app.MQC("/testmqc/proc3", NewObjNoneError, "service:queue3")
 	app.MQC("/testmqc/proc4", NewObjWithError, "service:queue4")
---
+++ test/demos/cronserver1/service.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/micro-plat/hydra/context"
+)
+
+//----------
+type mqcService struct {
+}
+
+func (s *mqcService) Handle(ctx context.IContext) interface{} {
+	return "object.success"
+}
+
+func FuncService(ctx context.IContext) interface{} {
+	return "func.success"
+}
+
+func NewObjNoneError() (*mqcService, error) {
+	return &mqcService{}, nil
+}
+
+func NewObjWithError() *mqcService {
+	return &mqcService{}
+}
+
+type cronService struct{}
+
+func (s *cronService) Handle(ctx context.IContext) interface{} {
+	fmt.Println("--cron")
+	debug.PrintStack()
+	//	queueObj := hydra.C.Queue().GetRegularQueue("mqcqueue")
+	//	return queueObj.Push("mqcservice:proc1", fmt.Sprintf(`{"xxx":"%d"}`, time.Now().Unix()))
+	return nil
+}
