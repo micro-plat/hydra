292a4c15302aab7de02ca041bda933121cbdada5
hj <546921722@qq.com>
2020-12-08 17:00:18 +0800

    测试修改

--- hydra/servers/http/router.http.go
+++ hydra/servers/http/router.http.go
@@ -24,7 +24,6 @@ func (s *Server) addHttpRouters(routers ...*router.Router) {
 	s.engine.Use(middleware.Delay().GinFunc())     //
 	s.engine.Use(middleware.Limit().GinFunc())     //限流处理
 	s.engine.Use(middleware.Static().GinFunc())    //处理静态文件
-	s.engine.Use(middleware.GinServiceExistsCheck(s.engine).GinFunc())
 	s.engine.Use(middleware.Header().GinFunc())    //设置请求头
 	s.engine.Use(middleware.Options().GinFunc())   //处理option响应
 	s.engine.Use(middleware.BasicAuth().GinFunc()) //
