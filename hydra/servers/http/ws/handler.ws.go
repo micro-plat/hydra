44a8c1a43e3682d2b6e32d46205b4212e7cfc163
zkfy <lib4net@163.com>
2020-12-08 16:15:00 +0800

    调整ws代码结构

---
+++ hydra/servers/http/ws/handler.ws.go
@@ -0,0 +1,51 @@
+package ws
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/micro-plat/hydra/components/ws"
+	"github.com/micro-plat/hydra/hydra/servers/pkg/middleware"
+)
+
+//upgrader 处理ws请求
+
+//WSExecuteHandler 业务处理Handler
+func WSExecuteHandler(service string) middleware.Handler {
+	return func(ctx middleware.IMiddleContext) {
+		n, ok := ctx.Meta().Get("__context_")
+		if !ok {
+			panic("ws获取context错误，未获取到__context_对象")
+		}
+		c := n.(*gin.Context)
+
+		conn, err := getUpgrader(c.Writer, c.Request, c.Request.Header)
+		if err != nil {
+			ctx.Response().Write(http.StatusNotAcceptable, fmt.Errorf("无法初始化ws.upgrader %w", err))
+			return
+		}
+
+		//构建处理函数
+		h := newWSHandler(conn, ctx.User().GetRequestID(), ctx.User().GetClientIP())
+		ws.WSExchange.Subscribe(ctx.User().GetRequestID(), h.recvNotify(c))
+		defer ws.WSExchange.Unsubscribe(ctx.User().GetRequestID())
+
+		//异步读取与写入
+		go h.readPump()
+		h.writePump()
+		ctx.Response().NoNeedWrite(c.Writer.Status())
+	}
+}
+func getUpgrader(w http.ResponseWriter, r *http.Request, h http.Header) (*websocket.Conn, error) {
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		Subprotocols:    []string{h.Get("Sec-WebSocket-Extensions")},
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	return upgrader.Upgrade(w, r, nil)
+}
---
+++ hydra/servers/http/ws/ws.msg.handler.go
@@ -0,0 +1,57 @@
+package ws
+
+import (
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/micro-plat/lib4go/logger"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+// wxHandler is a middleman between the websocket connection and the hub.
+type wsHandler struct {
+	engine    *wsEngine
+	conn      *websocket.Conn
+	closeChan chan struct{}
+	once      sync.Once
+	send      chan []byte
+	uuid      string
+	log       logger.ILogger
+	clientip  string
+}
+
+//newWSHandler 使用新引擎进行业务处理
+func newWSHandler(conn *websocket.Conn, uuid string, clientip string) *wsHandler {
+	if wsInternalEngine == nil {
+		panic("ws internal engine未初始化")
+	}
+	s := &wsHandler{
+		conn:      conn,
+		uuid:      uuid,
+		closeChan: make(chan struct{}),
+		send:      make(chan []byte, 256),
+		log:       logger.GetSession("ws", uuid),
+		clientip:  clientip,
+		engine:    wsInternalEngine,
+	}
+	return s
+}
