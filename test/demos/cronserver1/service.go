be8983b9fd08491ed6ba2cd410ffd5cd07de4acc
liujinyin <liujy@100bm.cn>
2020-12-08 09:34:37 +0800

    调整测试样例程序

--- test/demos/cronserver1/service.go
+++ test/demos/cronserver1/service.go
@@ -31,10 +31,10 @@ type cronService struct{}
 
 func (s *cronService) Handle(ctx context.IContext) interface{} {
 	obj := hydra.C.Queue().GetRegularQueue("mqcqueue")
-	err := obj.Push("service:queue1", `{}`)
-	fmt.Println("Push1:", err)
-	err = obj.Push("service:queue6", `{}`)
-	fmt.Println("Push2:", err)
+	err := obj.Send("service:queue1", `{}`)
+	fmt.Println("Send1:", err)
+	err = obj.Send("service:queue6", `{}`)
+	fmt.Println("Send2:", err)
 	//	queueObj := hydra.C.Queue().GetRegularQueue("mqcqueue")
 	//	return queueObj.Push("mqcservice:proc1", fmt.Sprintf(`{"xxx":"%d"}`, time.Now().Unix()))
 	return nil
