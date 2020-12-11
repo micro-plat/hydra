eb23f75b0df0627e3e531fa889dad34a84ac4bfb
zkfy <lib4net@163.com>
2020-12-11 11:23:51 +0800

    修改响应码默认值问题

--- global/debug.go
+++ global/debug.go
@@ -4,6 +4,3 @@ package global
 //启动后服务器会打印详细的启动、运行日志。也会将详细的错误信息输出到client
 //禁用后，当发生异常时client只会看到`Internal Server Error`，不会看到详细的错误消息
 var IsDebug = false
-
-//IsDebugByCli 通过cli设置debug
-var IsDebugByCli = false
