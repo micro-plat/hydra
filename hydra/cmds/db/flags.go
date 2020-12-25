b7c493ad0146b963613decdf266224796d81313e
zkfy <lib4net@163.com>
2020-12-25 17:16:25 +0800

    添加db命令参数

--- hydra/cmds/db/flags.go
+++ hydra/cmds/db/flags.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/micro-plat/hydra/global"
 	"github.com/micro-plat/hydra/hydra/cmds/pkgs"
 	"github.com/urfave/cli"
 )
@@ -11,13 +12,18 @@ var skip bool
 //getInstallFlags 获取运行时的参数
 func getInstallFlags() []cli.Flag {
 	flags := pkgs.GetBaseFlags()
+	flags = append(flags, cli.BoolFlag{
+		Name:        "debug,d",
+		Destination: &global.FlagVal.IsDebug,
+		Usage:       `-调试模式，打印更详细的系统运行日志`,
+	})
 	flags = append(flags, cli.StringFlag{
 		Name:        "db",
 		Destination: &dbName,
 		Usage:       `-数据库节点名,注册中配置的数据库节点名`,
 	})
 	flags = append(flags, cli.BoolFlag{
-		Name:        "skip,s",
+		Name:        "skip",
 		Destination: &skip,
 		Usage:       `-跳过执行失败的SQL语句`,
 	})
