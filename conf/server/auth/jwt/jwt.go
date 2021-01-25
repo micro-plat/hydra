24d788fba5c463ac9e95ab9549aeed188aca4d1e
zkfy <lib4net@163.com>
2021-01-25 11:50:26 +0800

    过期时间减去一个小时

--- conf/server/auth/jwt/jwt.go
+++ conf/server/auth/jwt/jwt.go
@@ -136,7 +136,7 @@ func (j *JWTAuth) GetJWTForRspns(token string, expired ...bool) (string, string)
 
 //getExpireTime 获取jwt的超时时间
 func (j *JWTAuth) getExpireTime(expired bool) string {
-	expireTime := time.Now()
+	expireTime := time.Now().Add(time.Hour * -1)
 	if !expired {
 		expireTime = time.Now().Add(time.Duration(time.Duration(j.ExpireAt)*time.Second - 8*60*60*time.Second))
 	}
