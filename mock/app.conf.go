9913540b05892288380029ce634ad7237ce465fc
zkfy <lib4net@163.com>
2021-01-26 11:19:05 +0800

    去除GetCachedObject

---
+++ mock/app.conf.go
@@ -0,0 +1,40 @@
+package mock
+
+import (
+	"github.com/micro-plat/hydra"
+	"github.com/micro-plat/hydra/conf/app"
+	"github.com/micro-plat/hydra/creator"
+	"github.com/micro-plat/hydra/global"
+	"github.com/micro-plat/hydra/hydra/servers/http"
+	"github.com/micro-plat/lib4go/types"
+)
+
+//NewAPPConf 构建APP配置
+func NewAPPConf(opts ...hydra.Option) (app.IAPPConf, error) {
+	for _, opt := range opts {
+		opt()
+	}
+	//初始化参数
+	global.Def.PlatName = types.GetString(global.Def.PlatName, "mock_plat")
+	global.Def.SysName = types.GetString(global.Def.SysName, "tserver")
+	global.Def.ClusterName = types.GetString(global.Def.ClusterName, "test")
+	global.Def.RegistryAddr = types.GetString(global.Def.RegistryAddr, "lm://.")
+	global.Def.ServerTypes = []string{http.API}
+
+	//发布配置
+	err := creator.Conf.Pub(global.Current().GetPlatName(),
+		global.Current().GetSysName(),
+		global.Current().GetClusterName(),
+		global.Def.RegistryAddr,
+		true)
+	if err != nil {
+		panic(err)
+	}
+
+	//初始化缓存
+	err = app.PullAndSave()
+	if err != nil {
+		panic(err)
+	}
+	return app.Cache.GetAPPConf(http.API)
+}
