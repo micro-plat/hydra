f9027f3972285086cc621e59f3817bc197b37a50
zkfy <lib4net@163.com>
2021-01-28 11:47:50 +0800

    修改rpc响应返回值为多次json.Marshal后的字符串问题

---
+++ pkgs/rspns_test.go
@@ -0,0 +1,16 @@
+package pkgs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRPCResponse(t *testing.T) {
+	//text := `{"channel_no":"testdownchannel","failed_code":"000","failed_msg":"订单成功","order_id":21013630,"request_no":"202002281414911911","status":"SUCCESS"}`
+
+	text := `"{\"channel_no\":\"testdownchannel\",\"failed_code\":\"000\",\"failed_msg\":\"订单成功\",\"order_id\":21013630,\"request_no\":\"202002281414911911\",\"status\":\"SUCCESS\"}"`
+	resp := NewRspnsByHD(200, ``, text)
+	assert.Equal(t, nil, resp.err)
+	assert.Equal(t, 3, len(resp.GetMap()))
+}
