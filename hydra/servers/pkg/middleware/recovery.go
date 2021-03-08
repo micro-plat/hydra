11672b89c071401fdc0284f2807c814d59060c6a
zkfy <lib4net@163.com>
2021-03-08 14:42:43 +0800

    添加recovery注释

--- hydra/servers/pkg/middleware/recovery.go
+++ hydra/servers/pkg/middleware/recovery.go
@@ -8,6 +8,7 @@ import (
 )
 
 //Recovery 用于处理请求过程中出现的非预见的错误
+//服务器首个Recovery中间件，应使用 Recovery(true)才能正确释放context资源
 func Recovery(needPrt ...bool) Handler {
 	return func(ctx IMiddleContext) {
 		defer func() {
