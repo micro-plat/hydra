e473af0a4dd125a57523dd2a7eacd793eedcd395
zkfy <lib4net@163.com>
2021-04-07 22:50:33 +0800

    更新逻辑

--- hydra/cmds/conf/encrypt.go
+++ hydra/cmds/conf/encrypt.go
@@ -3,16 +3,15 @@ package conf
 import (
 	"fmt"
 
-	"github.com/micro-plat/hydra/conf"
+	"github.com/micro-plat/hydra/conf/pkgs/security"
 	"github.com/urfave/cli"
 )
 
 func encrypt(c *cli.Context) (err error) {
-	cipherData := conf.Encrypt([]byte(orgData))
+	cipherData := security.Encrypt([]byte(orgData))
 	fmt.Println("原始内容：")
 	fmt.Println(orgData)
 	fmt.Println("加密结果：")
 	fmt.Println(cipherData)
 	return nil
-
 }
