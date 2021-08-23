74b5cfbe492ab3f512c867fe2add676fed6e6adf
hj <546921722@qq.com>
2021-08-23 14:34:25 +0800

    修改basic配置

--- conf/server/auth/basic/auth_test.go
+++ conf/server/auth/basic/auth_test.go
@@ -14,13 +14,13 @@ import (
 func Test_newAuthorization(t *testing.T) {
 	tests := []struct {
 		name string
-		args map[string]string
+		args []*member
 		want []*auth
 	}{
-		{name: "1. 创建空字符串验证对象", args: map[string]string{"": ""}, want: []*auth{&auth{userName: "", auth: createAuth("", "")}}},
-		{name: "2. 创建特殊符号的验证对象", args: map[string]string{"!@#$%*&^": ")(*^&&^@#$%"}, want: []*auth{&auth{userName: "!@#$%*&^", password: ")(*^&&^@#$%", auth: createAuth("!@#$%*&^", ")(*^&&^@#$%")}}},
-		{name: "3. 创建正常字符串的验证对象", args: map[string]string{"taosyteat": "123456"}, want: []*auth{&auth{userName: "taosyteat", password: "123456", auth: createAuth("taosyteat", "123456")}}},
-		{name: "4. 创建多个帐号的验证对象", args: map[string]string{"t1": "123", "t1_34": "$%$#12345"}, want: []*auth{&auth{userName: "t1", password: "123", auth: createAuth("t1", "123")}, &auth{userName: "t1_34", password: "$%$#12345", auth: createAuth("t1_34", "$%$#12345")}}},
+		{name: "1. 创建空字符串验证对象", args: []*member{&member{UserName: "", Password: ""}}, want: []*auth{&auth{userName: "", auth: createAuth("", "")}}},
+		{name: "2. 创建特殊符号的验证对象", args: []*member{&member{UserName: "!@#$%*&^", Password: ")(*^&&^@#$%"}}, want: []*auth{&auth{userName: "!@#$%*&^", password: ")(*^&&^@#$%", auth: createAuth("!@#$%*&^", ")(*^&&^@#$%")}}},
+		{name: "3. 创建正常字符串的验证对象", args: []*member{&member{UserName: "taosyteat", Password: "123456"}}, want: []*auth{&auth{userName: "taosyteat", password: "123456", auth: createAuth("taosyteat", "123456")}}},
+		{name: "4. 创建多个帐号的验证对象", args: []*member{&member{UserName: "t1", Password: "123"}, &member{UserName: "t1_34", Password: "$%$#12345"}}, want: []*auth{&auth{userName: "t1", password: "123", auth: createAuth("t1", "123")}, &auth{userName: "t1_34", password: "$%$#12345", auth: createAuth("t1_34", "$%$#12345")}}},
 	}
 	for _, tt := range tests {
 		got := newAuthorization(tt.args)
