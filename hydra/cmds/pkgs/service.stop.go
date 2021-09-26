b3f432df9c6dcff3b2f94b58657ad21bee2c706f
taoshouyin <527503855@qq.com>
2021-09-26 18:53:30 +0800

    修改程序退出时关闭容器中的组件

--- hydra/cmds/pkgs/service.stop.go
+++ hydra/cmds/pkgs/service.stop.go
@@ -3,11 +3,12 @@
  * @Autor: taoshouyin
  * @Date: 2020-12-23 15:43:54
  * @LastEditors: taoshouyin
- * @LastEditTime: 2021-09-26 16:55:44
+ * @LastEditTime: 2021-09-26 17:57:50
  */
 package pkgs
 
 import (
+	"github.com/micro-plat/hydra/components"
 	"github.com/micro-plat/hydra/global"
 	"github.com/micro-plat/hydra/hydra/cmds/pkgs/service"
 	"github.com/micro-plat/hydra/registry"
@@ -19,6 +20,11 @@ func (p *ServiceApp) Stop(s service.Service) (err error) {
 	globalLogger := global.Def.Log()
 	//8. 关闭服务器释放所有资源
 	globalLogger.Info(global.AppName, "正在退出...")
+
+	//关闭所有组件
+	if err := components.Def.Container().Close(); err != nil {
+		globalLogger.Error("关闭容器中组件失败:", err)
+	}
 
 	if p.server != nil {
 		//if !reflect.ValueOf(p.server).IsNil() {
