609e87e5b3bbbc28435fcb557e57da43932a465a
zkfy <lib4net@163.com>
2021-12-16 21:18:26 +0800

    添加图片服务

---
+++ hydra/servers/pkg/nfs/internal/image.go
@@ -0,0 +1,79 @@
+package internal
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/nfnt/resize"
+)
+
+func ScaleImageByPath(p string, width, height, quality int) ([]byte, string, error) {
+
+	basePath := fmt.Sprintf("tmp/scale/%s%s", strings.Replace(p, "/", "|", -1), filepath.Ext(p))
+	if fileExist(basePath) {
+		ctp := fmt.Sprintf("image/%s", strings.Trim(filepath.Ext(p), "."))
+		data, err := file2Bytes(basePath)
+		return data, ctp, err
+	}
+	data, err := file2Bytes(p)
+	if err != nil {
+		return nil, "", fmt.Errorf("读取文件失败:%w %s", err, p)
+	}
+	buff, ctp, err := ScaleImage(data, width, height, quality)
+	if err != nil {
+		return nil, "", err
+	}
+	f, err := os.OpenFile(basePath, os.O_TRUNC|os.O_CREATE, 0666)
+	if err != nil {
+		return buff, ctp, nil
+	}
+	f.Write(buff)
+	f.Close()
+	return buff, ctp, nil
+}
+
+func ScaleImage(data []byte, width, height, quality int) ([]byte, string, error) {
+	origin, ftp, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		return nil, "", err
+	}
+	if width == 0 || height == 0 {
+		width = origin.Bounds().Max.X
+		height = origin.Bounds().Max.Y
+	}
+	if quality == 0 {
+		quality = 20
+	}
+	canvas := resize.Thumbnail(uint(width), uint(height), origin, resize.Lanczos3)
+
+	buf := bytes.Buffer{}
+	contentType := "image/jpeg"
+	switch strings.ToLower(ftp) {
+	case "jpg", "jpeg":
+		contentType = "image/jpeg"
+		err = jpeg.Encode(&buf, canvas, &jpeg.Options{Quality: quality})
+	case "png":
+		contentType = "image/png"
+		err = png.Encode(&buf, canvas)
+	case "gif":
+		contentType = "image/gif"
+		err = gif.Encode(&buf, canvas, &gif.Options{})
+	default:
+		return data, contentType, nil
+	}
+
+	if err != nil {
+		return nil, "", err
+	}
+	if buf.Len() > len(data) {
+		return data, contentType, nil
+	}
+	return buf.Bytes(), contentType, nil
+}
