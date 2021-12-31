7aa7e2fc9eea8c8a6cad15b2653727816a6efae7
zkfy <lib4net@163.com>
2021-12-31 19:41:30 +0800

    更新current服务

--- hydra/servers/pkg/nfs/current.go
+++ hydra/servers/pkg/nfs/current.go
@@ -1,32 +1,36 @@
 package nfs
 
-//Save 保存文件到本地NFS服务路径
-// func Save(name string, buff []byte) (string, error) {
-// 	if currentModule == nil || currentModule.Done {
-// 		return "", fmt.Errorf("本地nfs未初始化或已关闭")
-// 	}
-// 	fp, domain, err := currentModule.SaveNewFile("", name, buff)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	rpath := fmt.Sprintf("%s/%s", strings.Trim(domain, "/"), strings.Trim(fp.Path, "/"))
-// 	return rpath, nil
-// }
+import (
+	"fmt"
+	"strings"
+)
+
+// Save 保存文件到本地NFS服务路径
+func Save(name string, buff []byte) (string, error) {
+	if currentModule == nil {
+		return "", fmt.Errorf("本地nfs未初始化或已关闭")
+	}
+	fp, domain, err := currentModule.Save("", name, buff)
+	if err != nil {
+		return "", err
+	}
+	rpath := fmt.Sprintf("%s/%s", strings.Trim(domain, "/"), strings.Trim(fp, "/"))
+	return rpath, nil
+}
 
 //Exists 检查本地和远程文件是否存在
 func Exists(name string) bool {
+	if currentModule == nil {
+		return false
+	}
 	return currentModule.Exists(name)
 }
 
-// //Download 下载文件
-// func Download(name string) ([]byte, error) {
-// 	if err := currentModule.CheckAndDownload(name); err != nil {
-// 		return nil, err
-// 	}
-// 	fs, err := http.FS(currentModule.Local).Open(name)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer fs.Close()
-// 	return ioutil.ReadAll(fs)
-// }
+//Download 下载文件
+func Download(name string) ([]byte, error) {
+	if currentModule == nil {
+		return nil, fmt.Errorf("本地nfs未初始化或已关闭")
+	}
+	buff, _, err := currentModule.Get(name)
+	return buff, err
+}
