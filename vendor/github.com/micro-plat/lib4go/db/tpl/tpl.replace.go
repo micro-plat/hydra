d8463d20f94d094669cf1cc2e7d49ed2d17ef1d9
zkfy <lib4net@163.com>
2024-02-04 15:02:29 +0800

    更新lib4go

--- vendor/github.com/micro-plat/lib4go/db/tpl/tpl.replace.go
+++ vendor/github.com/micro-plat/lib4go/db/tpl/tpl.replace.go
@@ -1,21 +1,24 @@
 package tpl
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 var patterns = map[string]string{
 	`where[\t]*[\n]*[\s]*order by`:                   "order by",
 	`where[\t]*[\n]*[\s]*group by`:                   "group by",
-	`where[\t]*[\n]*[\s]*limit\b`:                      "limit",
+	`where[\t]*[\n]*[\s]*limit\b`:                    "limit",
 	`where[\t]*[\n]*[\s]*or$`:                        "",
 	`where[\t]*[\n]*[\s]*and$`:                       "",
 	`where[\t]*[\n]*[\s]*or[\t]*[\n]*[\s]*order by`:  "order by",
 	`where[\t]*[\n]*[\s]*or[\t]*[\n]*[\s]*group by`:  "group by",
-	`where[\t]*[\n]*[\s]*or[\t]*[\n]*[\s]*limit\b`:     "limit",
-	`where[\t]*[\n]*[\s]*or[\t]*[\n]*[\s]*having\b`:    "having",
+	`where[\t]*[\n]*[\s]*or[\t]*[\n]*[\s]*limit\b`:   "limit",
+	`where[\t]*[\n]*[\s]*or[\t]*[\n]*[\s]*having\b`:  "having",
 	`where[\t]*[\n]*[\s]*and[\t]*[\n]*[\s]*order by`: "order by",
 	`where[\t]*[\n]*[\s]*and[\t]*[\n]*[\s]*group by`: "group by",
-	`where[\t]*[\n]*[\s]*and[\t]*[\n]*[\s]*limit\b`:    "limit",
-	`where[\t]*[\n]*[\s]*and[\t]*[\n]*[\s]*having\b`:   "having",
+	`where[\t]*[\n]*[\s]*and[\t]*[\n]*[\s]*limit\b`:  "limit",
+	`where[\t]*[\n]*[\s]*and[\t]*[\n]*[\s]*having\b`: "having",
 	`where[\t]*[\n]*[\s]*or[\t|\n|\s]+`:              "where ",
 	`where[\t]*[\n]*[\s]*and[\t|\n|\s]+`:             "where ",
 	`where[\t]*[\n]*[\s]*$`:                          "",
@@ -30,6 +33,7 @@ func replaceSpecialCharacter(s string) string {
 			return v
 		})
 	}
-	return result
+
+	return strings.ReplaceAll(result, "@~", "`")
 
 }
